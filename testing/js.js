db93734bfc42f5eeaa37e807eace9c5d85093817
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-09-21 02:19:27 -0700

    now can debug with typescript without compiling to .js first

--- testing/js.js
+++
@@ -1,5 +0,0 @@
-console.log('this is js.js');
-console.log('so how do you make this work anyway?');
-const hey002 = (...params) => {
-  console.log(params);
-};
--- testing/ts.ts
+++ testing/ts.ts
@@ -1,5 +1,7 @@
 console.log('is this working');
 console.log('so how do you make this work anyway?');
 const hey = (...params) => {
-  console.log(params);
+  console.log(params.join(''));
 };
+
+hey('alsdkjflakjdfkj');
