8c69710ff4ef5f732c6bdef9c5a8e08a9d288688
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-09-23 00:36:05 -0700

    add function to get clock wise degree

--- algorithms/rotating matrix/main.ts
+++ algorithms/rotating matrix/main.ts
@@ -47,33 +47,45 @@ function flipHorizontally(matrix) {
 function flipUpwardDiagonally(matrix: Array<Array<number>>) {
   const matrixClone = matrix.map((row) => row.slice());
   const matrixLength = matrixClone.length;
-  for (let rowIndex = 0; rowIndex < matrixLength ; rowIndex++) {
-    let lastCellIndexToVisit = matrixLength - 1 - rowIndex;
-    for (let cellIndex = 0; cellIndex < lastCellIndexToVisit; cellIndex++) {
-      const curFocusedCell = matrixClone[rowIndex][cellIndex];
-      const targetRowIndex = matrixLength - rowIndex;
-      const targetCellIndex = matrixLength - cellIndex;
-      matrixClone[rowIndex][cellIndex] = matrixClone[targetRowIndex][targetCellIndex];
-      matrixClone[targetRowIndex][targetCellIndex] = curFocusedCell;
+  for (let rowIndex = 0; rowIndex < matrixLength; rowIndex++) {
+    for (let cellIndex = 0; cellIndex < matrixLength; cellIndex++) {
+      let lastCellIndexToVisit = matrixLength - 1 - rowIndex;
+      if (cellIndex < lastCellIndexToVisit) {
+        const curFocusedCell = matrixClone[rowIndex][cellIndex];
+        const targetCellIndex = matrixLength - 1 - rowIndex;
+        const targetRowIndex = matrixLength - 1 - cellIndex;
+        matrixClone[rowIndex][cellIndex] = matrixClone[targetRowIndex][targetCellIndex];
+        matrixClone[targetRowIndex][targetCellIndex] = curFocusedCell;
+      }
     }
   }
-
   return matrixClone;
-}
- 
-function rotateMatrixBy90Deg(matrix) {
-  //first flip the rows
-  let flipedMatrix = flipVertically(matrix);
-  // flip downward diagonal
-  flipedMatrix = flipUpwardDiagonally(flipedMatrix);
-  return flipedMatrix;
 }
 
 const testMatrix1 = [
-  [1,  2,  3,  4],
-  [5,  6,  7,  8],
-  [9,  10, 11, 12],
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
   [13, 14, 15, 16]
 ];
 
-rotateMatrixBy90Deg(testMatrix1);
+/**
+ * give a matrix and a degree to rotate the matrix
+ * what if the number is negative? what if the number isn't some like 90, 180, 270
+ * what if the number is larger than 360
+ * 
+ * so normalize the deg to one of these four integers 0/90/180/270 first
+ * num % 1 == 0?
+ * num % 360 to get a number between -360 and 360
+ * then 360 + that number, you will get 0/90/180/270
+ */
+
+function getClockWiseRotationDegree(num) {
+  // get a number between -360 and 360;
+  const scaledDownNum = num % 360
+  // scaledDownNum might be a negative number, turn it into it's positive counter part
+  const clockWiseDegree = 360 + scaledDownNum;
+  return clockWiseDegree;
+}
+
+getClockWiseRotationDegree(-270);
