7a2a991fc9fb27e2ed830a666d784d5335363c5e
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-09-25 00:36:28 -0700

    add new solution that doesn't require using division

--- algorithms/products except self/main.ts
+++ algorithms/products except self/main.ts
@@ -19,7 +19,7 @@ function productsWithoutSelfWithDivision(numbers: Array<number>): Array<number> 
 
 const numbers = [1, 2, 3, 4, 5];
 
-productsWithoutSelfWithDivision(numbers);
+// productsWithoutSelfWithDivision(numbers);
 
 /**
  * without using division:
@@ -35,5 +35,38 @@ productsWithoutSelfWithDivision(numbers);
  * you can have an accumulative partial products for the left side of the triangle, save each at new array at the same index
  * as the iteraction
  * then iteract from bottom and up and do the same, mutiple all the partial products at the same index, done
+ * also, for current index, just replace that number with 1 and multiple
  * 
  */
+
+function productsWithoutSelfWithoutDivision(numbers: Array<number>): Array<number> {
+  const leftTriangleNums = [];
+  let leftAccumulator = 1;
+  // iteracting downward
+  for (let i = 0; i < numbers.length; i++) {
+    const previousNum = i - 1 < 0 ? 1 : numbers[i - 1];
+    leftAccumulator = leftAccumulator * previousNum;
+    leftTriangleNums.push(leftAccumulator);
+  }
+
+  // iteracting upward but reversing it first
+  const reversedNums = numbers.reverse();
+  let rightAccumulator = 1;
+  let rightTriangleNums = [];
+  for (let i = 0; i < reversedNums.length; i++) {
+    const previousNum = i - 1 < 0 ? 1 : reversedNums[i - 1];
+    rightAccumulator = rightAccumulator * previousNum;
+    rightTriangleNums.push(rightAccumulator);
+  }
+  // reversing the rightTriangleNumbs for merging with leftTriangleNums later
+  rightTriangleNums = rightTriangleNums.reverse();
+
+  const products = leftTriangleNums.map((leftPartialNum, index) => {
+    const rightPartialNum = rightTriangleNums[index];
+    return leftPartialNum * rightPartialNum;
+  })
+
+  return products;
+}
+
+productsWithoutSelfWithoutDivision(numbers);
