f4f3d0ae05447aa8af9404dc35388405bc4257ab
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-09-24 23:53:36 -0700

    add productsWithoutSelfWithDivision

--- algorithms/products except self/main.ts
+++ algorithms/products except self/main.ts
@@ -3,4 +3,20 @@
  * of the entire input array numbers without the one at that index
  * ex: [1, 2, 3, 4, 5]
  * return [120, 60, 40, 30, 24]
+ * what if you can't use division?
+ * 
+ * 
+ * solution one, can use division:
+ * simply get the product of the entire array, then map the input array by diving product by
+ * each number at each index
  */
+
+function productsWithoutSelfWithDivision(numbers: Array<number>): Array<number> {
+  const product = numbers.reduce((product, num) => product * num);
+  const products = numbers.map((num) => product / num );
+  return products
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+productsWithoutSelfWithDivision(numbers);
