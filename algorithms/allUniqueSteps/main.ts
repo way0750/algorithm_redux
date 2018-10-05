d07e4d8d23ac2019b03c220bb2cb13b8097e4dce
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-04 20:00:12 -0700

    add new charllenge: allUniqueSteps

---
+++ algorithms/allUniqueSteps/main.ts
@@ -0,0 +1,31 @@
+// There exists a staircase with N steps
+// and you are given type of positive steps you can make at a time, ex: 1 steps, 2 steps, 7 steps
+// Given N, write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+
+// For example, if N is 4, and you can only make 1 and 2 steps then there are 5 unique ways:
+// 1, 1, 1, 1
+// 2, 1, 1
+// 1, 2, 1
+// 1, 1, 2
+// 2, 2
+
+/**
+ * If you can make lets 2 and 3 steps, and N = 100
+ * if you make 2, 2, then 2 steps, that's total of 6 steps made and 94 left
+ * if you mkae 3, 3 steps, that's total of 6 steps made also, and also 94 teps left
+ * so in searching all possible unique stepping patterns, there must be some calculations that bould be reused.
+ * use caching!
+ * 
+ * solution 1:
+ * use dynamic programming
+ * we can building case from 0 step all the way to n steps, and save each step in the cache
+ * then return it at the end with cache of n step
+ * 
+ * and here is how we can seach for all possible patterns in each n
+ * using recursion:
+ * basecase: if input n is 0 return [[]] meaning a list of 1 possible patterns has been found
+ * what to always return: an array of sub array: a collection of sub patterns
+ * what to do with those sub patterns: put the current stepping option in front of the sub patter
+ * how to make problem smaller:
+ *   smaller in terms of N, so passing N - steppingOptionNum to the next recursive call
+ */
