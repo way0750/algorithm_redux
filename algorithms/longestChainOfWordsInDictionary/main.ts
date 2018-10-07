884e84add662f85c81294c6c1c36c7f8b70b78f6
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-06 18:13:05 -0700

    found solution by using graph

--- algorithms/longestChainOfWordsInDictionary/main.ts
+++ algorithms/longestChainOfWordsInDictionary/main.ts
@@ -8,4 +8,26 @@
  * ex: [ab, bka] this is't a chain, because kba, even though it has both ab from prevous
  * word, the order isn't maintained
  * [ab, akb], this is a chain, akb has both 'a' and 'b', and the order is maintained in akb
+ * 
+ * solution 1:
+ * put all words in the list into a hash, then loop through each word in the input list.
+ * input: [spring, in, sin, spin, cat, day, dog, food, sping, shoes]
+ * put all of them in a hash :
+ * {
+      spring: {children: [], longestChain: []},
+      in: {children: [], longestChain: []},
+      sin: {children: [], longestChain: []},
+      spin: {children: [], longestChain: []},
+      cat: {children: [], longestChain: []},
+      day: {children: [], longestChain: []},
+      dog: {children: [], longestChain: []},
+      food: {children: [], longestChain: []},
+      sping: {children: [], longestChain: []},
+      shoes:{children: [], longestChain: []} 
+ * }
+ * 
+ * you essentially end up with a bunch of graphs
+ * then do a loop through the input list and link all the words in the graph
+ * then do a depth first seach and concate longestChains together
+ * and return the longest one.
  */
