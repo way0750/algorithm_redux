0bfbfce1ef630bb54cd7f9fc9a807c4576508b9c
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-07 20:06:56 -0700

    add tree path

---
+++ algorithms/TreePathSum/main.ts
@@ -0,0 +1,23 @@
+/**
+ * given a number and binary tree, return a boolean for if it is possible to have a root to leaf path sum up to a given number k
+ * ex:     
+ *     8
+ *    / \
+ *   10  8
+ * 
+ * k = 18
+ * then you should return true for that
+ * 
+ * solution 1:
+ * just do a depth first search from root to leaf and then compare if the sum from root to leaf is === to k
+ * 
+ * can do this recursively
+ * base case: node is null/undefined and accumlative value is same as k
+ *   return true, else false
+ * what to always return: boolean
+ * what to do with return: if true, just keep returning it, if it is false, then keep search children
+ * how to make problem smaller: pass self value to recursive call on children
+ */
+
+
+
