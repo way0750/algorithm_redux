43d1b4c5f77fb875911c76c6000c62f90b30a4ef
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-07 20:48:33 -0700

    add solution for meeting rooms overlap

--- algorithms/meetingRoomOverlap/main.ts
+++ algorithms/meetingRoomOverlap/main.ts
@@ -16,3 +16,21 @@
  * from previous meeting ending time
  * if it is earlier than previous meeting ending time. then nope
  */
+
+function canAttendAllMeetings(meetings: Array<{start: number, end: number}>): boolean {
+  const sortedMeetings = meetings.sort((meet1, meet2) => {
+    if (meet1.start === meet2.start) {
+      return 0;
+    } else {
+      return meet1.start > meet2.start ? 1 : -1;
+    }
+  });
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    const previousMeeting = sortedMeetings[i-1];
+    const curMeeting = sortedMeetings[i];
+    if (curMeeting.start > previousMeeting.end) {
+      return false;
+    }
+  }
+  return true;
+}
