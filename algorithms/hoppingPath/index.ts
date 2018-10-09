3f76f83d77f338aa80a0c659575ecd9e2df39b3a
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-08 21:49:59 -0700

    add solution for hoppingPath

--- algorithms/hoppingPath/index.ts
+++ algorithms/hoppingPath/index.ts
@@ -5,4 +5,15 @@
  * at index 0, there is 2, that means you can hop forward 0/1/2 steps
  * if you hop 2 steps forward, you will land on index 2 with a integer of 1 which if you take number and hop
  * forward you will end up on index 3 which is the last index. return true for this config
+ * 
+ * 
+ * 
+ * solution 1
+ * if you have an array like this:
+ * [4, 3, 0, 0, 1, 0];
+ *  0, 1, 2, 3, 4, 5
+ * Instead of brute force it by coming up all the possible path and see if at lease 1 of them
+ * can make it to th end, if you go backward and find out if certain index can make it to the end, then you
+ * when go backward all the way to the first index, you will need very little computation and see if
+ * any stepping will make it to an index with known valid path
  */
