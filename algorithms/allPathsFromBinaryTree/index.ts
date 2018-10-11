b842f2677f3c7114496f8bbac0ab4b14eda27c9a
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-10 23:26:33 -0700

    add new problem

---
+++ algorithms/allPathsFromBinaryTree/index.ts
@@ -0,0 +1,8 @@
+// Given a binary tree, return all paths from the root to leaves.
+// For example, given the tree (excuse the formatting, 4 and 5 branch from 3):
+//  1
+// / \
+// 2 3
+//  / \
+//  4 5
+// It should return [[1, 2], [1, 3, 4], [1, 3, 5]]
