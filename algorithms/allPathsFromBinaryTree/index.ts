0825f370b346873edf49c6039a2ba83b054d593c
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-10 23:38:37 -0700

    add solution

--- algorithms/allPathsFromBinaryTree/index.ts
+++ algorithms/allPathsFromBinaryTree/index.ts
@@ -6,3 +6,30 @@
 //  / \
 //  4 5
 // It should return [[1, 2], [1, 3, 4], [1, 3, 5]]
+
+/**
+ * solution 1: recursion
+ * do a depth first search and return all value from leaf to root
+ * 
+ * base case: if node has not children at all
+ *   return [[self.value]], as a collection of just one path which is consisted just the value of the leaf node
+ *   but later on as the call stack returns, values from higher up on the tree will be prepended to each path
+ * what to always return: an array of sub-arrays, each sub-array is a path
+ * what to do with each return, add current node value to each and every single one of the sub array
+ * how to make problem smaller: call each one of the node's children
+ */
+
+export function depthFirstSearch(node) {
+  // to deal with edge case when no node is passed in
+  if (!node) {
+    return [];
+  } else if (!node.left && !node.right) {
+    // when no child at all, return [[ self value ]];
+    return [[node.value]];
+  }
+  const leftPaths = depthFirstSearch(node.left);
+  const rightPaths = depthFirstSearch(node.right);
+  const allPaths = [...leftPaths, ...rightPaths];
+  // add current node value to all sub paths
+  return allPaths.map((path) => [node.value, ...path]);
+};
