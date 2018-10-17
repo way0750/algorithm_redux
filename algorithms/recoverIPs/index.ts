6d7238d8a31312fe07e24a6fa890515b894a85af
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-17 02:59:37 -0700

    didn't end up using cache

--- algorithms/recoverIPs/index.ts
+++ algorithms/recoverIPs/index.ts
@@ -1,3 +1,5 @@
+import { getMaxListeners } from "cluster";
+
 // Given a string containing only digits, restore it by returning all possible valid IP address combinations.
 // Example:
 // Input: "25525511135"
@@ -39,16 +41,71 @@
 export function findAllIPs(numStr: string, minStrLength: number = 3, maxStrLength: number = 9): Array<string> {
   let patterns = [];
   let strSlicingCount = 1;
-  while (strSlicingCount < 4) {
-    const curNum = Number(numStr.slice(0, strSlicingCount));
+  while (strSlicingCount <= Math.min(3, numStr.length)) {
+    const curNumStr = numStr.slice(0, strSlicingCount)
+    const curNum = Number(numStr ? curNumStr : NaN);
     let remainingNum = numStr.slice(strSlicingCount);
     let isEnoughtNumLeft = remainingNum.length >= minStrLength && remainingNum.length <= maxStrLength;
-    if (curNum > -1 && curNum < 256 && isEnoughtNumLeft) {
+    const isCurNumWithinRange = curNum > -1 && curNum < 256;
+    if (isCurNumWithinRange && remainingNum.length === 0 && !minStrLength && !maxStrLength) {
+      patterns.push(`${curNumStr}`);
+    } else if (isCurNumWithinRange && isEnoughtNumLeft) {
       let subPatterns = findAllIPs(remainingNum, minStrLength - 1, maxStrLength - 3);
-      subPatterns = subPatterns.map((subPattern) => `${curNum}.${subPattern}`);
+      subPatterns = subPatterns.map((subPattern) => `${curNumStr}.${subPattern}`);
       patterns.push(...subPatterns);
     }
     strSlicingCount++;
   }
   return patterns;
 }
+
+describe('recover all IPs', () => {
+  it('should return for the test example above', () => {
+    const numStr = "25525511135";
+    const expectedReturn = ["255.255.11.135", "255.255.111.35"];
+    expect(findAllIPs(numStr)).to.deep.equal(expectedReturn);
+  });
+  it('should return the right amount', () => {
+    const numStr = "255255255255";
+    const expectedReturn = ["255.255.255.255"];
+    expect(findAllIPs(numStr)).to.deep.equal(expectedReturn);
+  });
+  it('should return the right amount too', () => {
+    const numStr = "101010101";
+    const expectedReturn = [
+      "1.01.010.101",
+      "1.010.10.101",
+      "1.010.101.01",
+      "10.1.010.101",
+      "10.10.10.101",
+      "10.10.101.01",
+      "10.101.0.101",
+      "10.101.01.01",
+      "10.101.010.1",
+      "101.0.10.101",
+      "101.0.101.01",
+      "101.01.0.101",
+      "101.01.01.01",
+      "101.01.010.1",
+      "101.010.1.01",
+      "101.010.10.1",
+    ];
+
+    expect(findAllIPs(numStr)).to.deep.equal(expectedReturn);
+  });
+  it('should return empty array for super long string ', () => {
+    const numStr = "255255111351212121212";
+    const expectedReturn = [];
+    expect(findAllIPs(numStr)).to.deep.equal(expectedReturn);
+  });
+  it('should return empty array for string that is too short', () => {
+    const numStr = "251";
+    const expectedReturn = [];
+    expect(findAllIPs(numStr)).to.deep.equal(expectedReturn);
+  });
+  it('should return empty array for no possible IP found', () => {
+    const numStr = "999999999999";
+    const expectedReturn = [];
+    expect(findAllIPs(numStr)).to.deep.equal(expectedReturn);
+  });
+});
