369e68ed29cb3f732b9029474111888a879650d4
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-17 01:52:21 -0700

    add comment for solution

--- algorithms/recoverIPs/index.ts
+++ algorithms/recoverIPs/index.ts
@@ -2,3 +2,36 @@
 // Example:
 // Input: "25525511135"
 // Output: ["255.255.11.135", "255.255.111.35"]
+
+/**
+ * There will be reusable sub patterns, so use cache?
+ * And at the same time, you have to get all possible valid IP addresses, so use recursion to find all?
+ * 
+ * the interesting twist here is that at each section of the IP, there can only 1 to 3 numbers 
+ * so if the input is 9123456789123, there are 13 numbers, so you can't create a valid IP
+ * also, the max numeric value of each section is 0...255
+ * what if it contains 0?
+ *   IP address might not be allowed to start with 0
+ *   does that matter?
+ * 
+ * solution 1:
+ * loop through the string and take numbers from 1 to 3 digits to create 1 section of an IP
+ * it is a valid section if: the remaning string can create the right amount of sections 
+ *   ex: after taking 1 digit, we still need to create 3 sections, meaning we need between 3 to 9 numbers
+ *     less than 3 or more than 9 meanings taking 1 digit for this section will work
+ * it is a valid section if: the section number is between 0 and 255
+ * if it is a valid section, then recursively call with remaing string, with the stack number too so we know
+ *   how to check the length required for making sub sections
+ * 
+ * prepend current section to each of the returned pattern
+ * take more digit as new a number for this section
+ * 
+ * 
+ * the recursion setup:
+ * recursive case: if remaining string is between the passed in min and max length
+ *   and if the current section num is between 0, 255
+ * what to return always: an array of possible strings
+ * what to do with return, add current section num to each string's front
+ * how to make problem smaller:
+ *   recursively call with remaing string and new min (curMin - 1) and max length (curMax - 3)
+ */
