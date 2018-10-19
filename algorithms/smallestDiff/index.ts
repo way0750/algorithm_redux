a607288e629af8fcb46b34c65bca1321de1fbcf4
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-18 22:56:45 -0700

    add new challenge

---
+++ algorithms/smallestDiff/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Given two unsorted array of integers, positive or negative
+ * find two elements, one from each array, such that the difference between them is the smallest possible amount
+ * these two arrays of integera
+ * ex: 
+ * arr1: [3,2,5]
+ * arr2: [4,7,2]
+ * should return 0 because arr1: 2 vs arr2: 2 is 0
+ */
