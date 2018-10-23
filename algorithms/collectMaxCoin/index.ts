8ffa1852d6d22fbe9c21a62a97bf9d4d7fab0348
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-22 19:00:45 -0700

    add solution

--- algorithms/collectMaxCoin/index.ts
+++ algorithms/collectMaxCoin/index.ts
@@ -1,6 +1,16 @@
-// You are given a 2-d matrix where each cell represents number of coins in that cell. Assuming we start at matrix[0][0], and can only move right or down, find the maximum number of coins you can collect by the bottom right corner.
+// You are given a 2-d matrix where each cell represents number of coins in that cell.
+// Assuming we start at matrix[0][0], and can only move right or down, find the maximum number of coins you
+// can collect by the bottom right corner.
 // For example, in this matrix
 // 0 3 1 1
 // 2 0 0 4
 // 1 5 3 1
 // The most we can collect is 0 + 2 + 1 + 5 + 3 + 1 = 12 coins.
+
+/**
+ * solution 1: use dynamic programming
+ * loop from the very last cell, to its far left, then same for the upper row one by one.
+ * In that case you are pretty much creating all possible sub case/problem all. And each sub case/problem
+ * can reuse result from its immedite right cell and immedite bottom cell. 
+ * at the very end, just return the value at matrix[0][0]
+ */
