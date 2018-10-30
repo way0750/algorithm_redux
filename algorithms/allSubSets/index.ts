3252dd9c456fec39554abacabbb4d4c54daf4fb9
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-10-29 18:51:57 -0700

    add solution

--- algorithms/allSubSets/index.ts
+++ algorithms/allSubSets/index.ts
@@ -1,3 +1,8 @@
-// This problem was asked by Google.
 // The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
 // For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+
+/**
+ * been there done that, all you got to do is make a copy of all current sub sets, add one element into each of the sub set copy
+ * repeat until you have gone through all the elements
+ * by default start with an empty set
+ */
