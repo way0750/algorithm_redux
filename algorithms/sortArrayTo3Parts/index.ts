4975a7b7c2b3bc10d29570490724ce17d1d9453a
Huiqiang (Way) Huang <bruce0750@gmail.com>
2018-11-13 22:39:57 -0800

    solution

--- algorithms/sortArrayTo3Parts/index.ts
+++ algorithms/sortArrayTo3Parts/index.ts
@@ -5,3 +5,11 @@
 // Ordering within a part can be arbitrary.
 // For example, given x = 10 and lst = [9, 12, 3, 5, 14, 10, 10], one partition may be [9, 3, 5, 10, 10, 12, 14]a
 
+/**
+ * solution, loop the array twice, from front to back and then back to front
+ * first loop, keep 2 pointers, one is next swappable index for a number that is smaller than x
+ *   and then a pointer to keep moving forward to check the value of each number against x
+ * second loop, keep 2 pointers, one is next swappable index for a number that is larger than x
+ *   set that pointer to last index
+ *   and then a pointer to keep moving backward to check the value of each number against x
+ */
