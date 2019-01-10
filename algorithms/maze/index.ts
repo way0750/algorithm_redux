75f702c2972559ec8725ab5d4a543d80d24f3754
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-01-10 00:03:35 -0800

    add solution

--- algorithms/maze/index.ts
+++ algorithms/maze/index.ts
@@ -19,43 +19,90 @@
 const GREEN = 'green';
 const RED = 'red';
 const YELLOW = 'yellow';
-export const edges = {
-  r1_1: { color: YELLOW, to: ['r3_2', 'r5_2', 'r4_1'] },
-  r1_2: { color: GREEN, to: ['r1_1', 'r3_3'] },
+export let nodes = {
+  r1_1: { visited: false, color: YELLOW, to: ['r3_2', 'r5_2', 'r4_1'] },
+  r1_2: { visited: false, color: GREEN, to: ['r1_1', 'r3_3'] },
 
-  r2_1: { color: YELLOW, to: ['r3_2', 'r5_2', 'r7_1'] },
-  r2_2: { color: GREEN, to: ['r2_1', 'r1_1', 'r5_1', 'r5_3'] },
+  r2_1: { visited: false, color: YELLOW, to: ['r3_2', 'r5_2', 'r7_1'] },
+  r2_2: { visited: false, color: GREEN, to: ['r2_1', 'r1_1', 'r5_1', 'r5_3'] },
 
-  r3_1: { color: RED, to: ['r2_2', 'r9_1'] },
-  r3_2: { color: RED, to: ['r2_2', 'r9_1', 'r6_2'] },
-  r3_3: { color: YELLOW, to: ['r4_1', 'r4_2'] },
+  r3_1: { visited: false, color: RED, to: ['r2_2', 'r9_1'] },
+  r3_2: { visited: false, color: RED, to: ['r2_2', 'r9_1', 'r6_2'] },
+  r3_3: { visited: false, color: YELLOW, to: ['r4_1', 'r4_2'] },
 
-  r4_1: { color: RED, to: ['r1_2', 'r5_4', 'r6_3'] },
-  r4_2: { color: RED, to: ['r1_2', 'r5_4', 'r5_5'] },
+  r4_1: { visited: false, color: RED, to: ['r1_2', 'r5_4', 'r6_3'] },
+  r4_2: { visited: false, color: RED, to: ['r1_2', 'r5_4', 'r5_5'] },
 
-  r5_1: { color: YELLOW, to: ['r3_2', 'r5_2', 'r7_1'] },
-  r5_2: { color: RED, to: ['r2_2', 'r6_1', 'r6_2'] },
-  r5_3: { color: YELLOW, to: ['r3_2', 'r4_1', 'r5_2'] },
-  r5_4: { color: GREEN, to: ['r5_3', 'r6_4'] },
-  r5_5: { color: GREEN, to: [] }, // this is the ending node so no need to connecto to any other nodes
+  r5_1: { visited: false, color: YELLOW, to: ['r3_2', 'r5_2', 'r7_1'] },
+  r5_2: { visited: false, color: RED, to: ['r2_2', 'r6_1', 'r6_2'] },
+  r5_3: { visited: false, color: YELLOW, to: ['r3_2', 'r4_1', 'r5_2'] },
+  r5_4: { visited: false, color: GREEN, to: ['r5_3', 'r6_4'] },
+  r5_5: { visited: false, color: GREEN, to: [] }, // this is the ending node so no need to connecto to any other nodes
 
-  r6_1: { color: GREEN, to: ['r5_1', 'r7_2', 'r8_1'] },
-  r6_2: { color: GREEN, to: ['r1_1', 'r2_1', 'r5_3', 'r7_2'] },
-  r6_3: { color: GREEN, to: ['r5_3', 'r8_3'] },
-  r6_4: { color: YELLOW, to: ['r4_2', 'r8_4'] },
+  r6_1: { visited: false, color: GREEN, to: ['r5_1', 'r7_2', 'r8_1'] },
+  r6_2: { visited: false, color: GREEN, to: ['r1_1', 'r2_1', 'r5_3', 'r7_2'] },
+  r6_3: { visited: false, color: GREEN, to: ['r5_3', 'r8_3'] },
+  r6_4: { visited: false, color: YELLOW, to: ['r4_2', 'r8_4'] },
 
-  r7_1: { color: RED, to: ['r2_2', 'r6_1', 'r9_1'] },
-  r7_2: { color: YELLOW, to: ['r7_1', 'r7_3'] },
-  r7_3: { color: RED, to: ['r6_2', 'r6_3', 'r7_4', 'r8_2'] },
-  r7_4: { color: GREEN, to: ['r6_4', 'r8_3'] },
+  r7_1: { visited: false, color: RED, to: ['r2_2', 'r6_1', 'r9_1'] },
+  r7_2: { visited: false, color: YELLOW, to: ['r7_1', 'r7_3'] },
+  r7_3: { visited: false, color: RED, to: ['r6_2', 'r6_3', 'r7_4', 'r8_2'] },
+  r7_4: { visited: false, color: GREEN, to: ['r6_4', 'r8_3'] },
 
-  r8_1: { color: YELLOW, to: ['r7_1', 'r9_2'] },
-  r8_2: { color: GREEN, to: ['r7_2'] },
-  r8_3: { color: YELLOW, to: ['r7_3', 'r9_3'] },
-  r8_4: { color: RED, to: ['r7_4', 'r9_4'] },
+  r8_1: { visited: false, color: YELLOW, to: ['r7_1', 'r9_2'] },
+  r8_2: { visited: false, color: GREEN, to: ['r7_2'] },
+  r8_3: { visited: false, color: YELLOW, to: ['r7_3', 'r9_3'] },
+  r8_4: { visited: false, color: RED, to: ['r7_4', 'r9_4'] },
 
-  r9_1: { color: GREEN, to: ['r2_1', 'r5_1', 'r8_1'] },
-  r9_2: { color: RED, to: ['r8_2', 'r9_1'] },
-  r9_3: { color: RED, to: ['r8_2', 'r9_4'] },
-  r9_4: { color: GREEN, to: ['r8_3'] },
+  r9_1: { visited: false, color: GREEN, to: ['r2_1', 'r5_1', 'r8_1'] },
+  r9_2: { visited: false, color: RED, to: ['r8_2', 'r9_1'] },
+  r9_3: { visited: false, color: RED, to: ['r8_2', 'r9_4'] },
+  r9_4: { visited: false, color: GREEN, to: ['r8_3'] },
 };
+
+// map each edge in the node.to to a node
+const nodeIds = Object.keys(nodes);
+nodeIds.forEach((nodeId) => {
+  nodes[nodeId].to = nodes[nodeId].to.map((edgeId: string) => {
+    return nodes[edgeId];
+  });
+});
+
+/**
+ * base case:
+ *   if current node is the end node return [current node]
+ *   if current node is visited simply return []
+ *     this will take care of the don't-visit-same-node-twice-in-a-row problem
+ *     prevent infinite loop, keep going to the same node isn't not going anywhere
+ * what to return 
+ *   always return an array potentially with nodes, it means a sub path
+ * what to do with return
+ *   compare with other sub path from sibilings, keep the shortest one
+ * how to make problem smaller/how to recursively call
+ *   just recursively call with each .to node
+ */
+
+export function findPath(curNode, endNode): Array<object> {
+  if (curNode === endNode) {
+    return [curNode];
+  } else if (curNode.visited) {
+    // dead end path
+    return [];
+  } else {
+    let curShortestSubPath = [];
+    curNode.visited = true;
+    curNode.to.forEach((edgeNode) => {
+      const subPath = findPath(edgeNode, endNode);
+      if (!curShortestSubPath.length) {
+        curShortestSubPath = subPath;
+      } else if (subPath.length && subPath.length < curShortestSubPath.length) {
+        curShortestSubPath = subPath;
+      }
+    });
+
+    if (curShortestSubPath.length) {
+      curShortestSubPath.unshift(curNode);
+    }
+    return curShortestSubPath;
+  }
+}
