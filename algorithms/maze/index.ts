0466924658958263a41223b46488b4c913080e7d
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-01-09 22:41:36 -0800

    add the maze problem

---
+++ algorithms/maze/index.ts
@@ -0,0 +1,12 @@
+// the problem is from here:
+// https://www.popsci.com/worlds-hardest-maze
+
+/**
+ * Essentially this is a graph traversal problem
+ * 
+ * constrains:
+ * you must traverse in this order: red green blue
+ * you can not go through the same node twice in a row
+ * you have to go from r3_1 to r5_5
+ * 
+ */
