4373c934e68183154a8cd6acf3dd61ca0b7f414d
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-01-15 22:46:42 -0800

    done implementing solution

--- algorithms/findNextlargerPermutation/index.ts
+++ algorithms/findNextlargerPermutation/index.ts
@@ -8,3 +8,28 @@
  * then reverse the entire right section
  * put everything back together into a number
  */
+
+export function findNextLargerPermutation(number: number): number {
+  const digits: Array<number> = number.toString().split('').map((numStr) => +numStr);
+  const swappableIndex = digits.reduce((index, curNum, curIndex) => {
+    const rigthDigit = digits[curIndex + 1];
+    return curNum < rigthDigit ? curIndex : index;
+  }, -1);
+
+  if (swappableIndex === -1) {
+    return NaN;
+  }
+
+  const swappableNum = digits[swappableIndex];
+  const toSwapIndex = digits.slice(swappableIndex + 1).reduce((toSwapIndex, curNum, curIndex) => {
+    return swappableNum < curNum ? curIndex : toSwapIndex;
+  }, -1);
+
+  digits[swappableIndex] = digits[toSwapIndex];
+  digits[toSwapIndex] = swappableNum;
+
+  const leftDigits = digits.slice(0, swappableIndex + 1);
+  const rigthDigit = digits.slice(swappableIndex).reverse();
+  const finalNumStr = [...leftDigits, ...rigthDigit].map((num) => `${num}`).join('');
+  return +finalNumStr;
+}
