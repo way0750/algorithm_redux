b5a9a39fb9e5c7d5010b5a5a402033808aaf983a
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-01-15 22:59:03 -0800

    update comment

--- algorithms/findNextlargerPermutation/index.ts
+++ algorithms/findNextlargerPermutation/index.ts
@@ -2,7 +2,7 @@
  * Given an integer, find the next permutation of it in absolute order.
  * For example, given 48975, the next permutation would be 49578.
  * 
- * solution: go from right to left and find the first digit that is smaller than the one on immediate right
+ * solution: go from left to right and find the last digit that is smaller than the one on immediate right
  * that is the first digit that you can switch with anything on the right
  * then swap with the smallest of the larger number on the right
  * then reverse the entire right section
