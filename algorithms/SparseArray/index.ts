24e4c012d2f39df49eb489f6f44611a9482662eb
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-02-05 23:35:26 -0800

    add solution

--- algorithms/SparseArray/index.ts
+++ algorithms/SparseArray/index.ts
@@ -6,4 +6,14 @@
  * same interface: init(arr, size): initialize with the original large array and
  * size. set(i, val): updates index at i with val. get(i): gets the value at
  * index i.
+ * 
+ * solution:
+ * 
+ * this is essentially how JavaScript implements Array:
+ * using a JSON object without space actually allocated for the array
+ * then when actually assigning a value to an index then in the background just
+ * add a property: by using the index as the property key, then maintain a
+ * length property for traversal purpose
+ * and whenever accessing an index that has yet been deliberately assigned, just
+ * return a default value: undefined, but in this case we can just number: 0;
  */
