2604a79bb4295032ba390ff7a878111b8394ded1
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-02-16 16:11:18 -0800

    add solution

--- algorithms/trimAll0SubTrees/index.ts
+++ algorithms/trimAll0SubTrees/index.ts
@@ -4,8 +4,36 @@
  *      0
  *     / \
  *    1   0
- *        / \
+ *       /  \
  *       1   0
  *      /\
  *     0  0
+ * 
+ * the tree should be trimmed into:
+ *      0
+ *     / \
+ *    1   0
+ *       /
+ *       1
+ * 
+ * 
+ * use recursion to check if sub tree is all 0s
+ * 
+ * base case
+ *    if no child at all 
+ *      if self value is 1 return return self
+ *    else self value would only be 0, that means this sub tree (with only 1
+ *    node, which is self node), is all 0, return null
+ * 
+ * what to do with return?????
+ *   meaning the left/right child has return itself, no need to do anything
+ *   because that node still the child node
+ *   if return is null, remove that child
+ * 
+ * what to always return:
+ *    always return object: node / null
+ * 
+ * how to make problem "smaller"/recursively call?
+ *   just call left and right child...
+ * 
  */
