f768d143dc5f5b01f70a596a8e72b7467e6fa463
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-02-16 16:03:55 -0800

    add exaple

---
+++ algorithms/trimAll0SubTrees/index.ts
@@ -0,0 +1,11 @@
+/**
+ * give a binary tree consisted of nodes that have only either 1 or 0 as value
+ * trim the tree of any sub tree that is consisted of only 0s.
+ *      0
+ *     / \
+ *    1   0
+ *        / \
+ *       1   0
+ *      /\
+ *     0  0
+ */
