9ecaab1b0adfc79b21f1c42348126bd45868dd27
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-06 00:19:49 -0700

    add solution

--- data_structures/arrays_and_string/stringCompression.ts
+++ data_structures/arrays_and_string/stringCompression.ts
@@ -4,4 +4,14 @@
  * would become a2b1c5a3. If the "compressed" string would not become smaller than
  * the original string, your method should return the original string.
  * You can assume the string has only uppercase and lowercase letters (a - z).
+ * 
+ * solution 1:
+ *  initialize curChar to '' and count to 0 and compressedStr to = ''
+ *  go through the string char by char
+ *  if curChar is '' set it to current character at current index and count to 1
+ *  if curChar is different at current character, then add count + curChar to compressedStr
+ *    then reset curChar to the new current character, and set count to 1
+ *     and compare length of compressedStr to input str, if longer then return inputStr
+ *  
+ *  at the very end, return the compressedStr;
  */
