56d60b0f1e960612936e2831e505b8c70126dc4f
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-06 01:37:04 -0700

    add solution

--- data_structures/arrays_and_string/zeroMatrix.ts
+++ data_structures/arrays_and_string/zeroMatrix.ts
@@ -1,4 +1,55 @@
 /**
  * Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
  * column are set to O.
+ * 
+ * solution 1, not in place:
+ * set zeroColRecord to the copy of first array;
+ * set allZero to same length array as the first row, but with everything set to
+ * 0
+ * loop through each array
+ * check each index for 0, if found, then update same index in zeroColRecord to
+ * be 0.
+ * if any 0 is found then make matrix refer to allZero for this row index
+ * 
+ * solution 2 in place:
+ * first find all the zero column
+ * and each row, if found 0, then change first element to 0
+ * 
+ * then second loop, check and see if first element is 0
+ * if yes: 0 out every element
+ * if no: then use the record
+ * 
+ * time and space:
+ * time: you will go through each element twice so 2N**2 which is n**2
+ * space: you will keep one row which M length, so M
  */
+
+export function zeroMatrixInPlace(matrix) {
+  if (matrix.length === 0 ) {
+    return matrix;
+  }
+  const zeroColRecord = [...(matrix[0] || [])];
+  matrix.forEach((row) => {
+    let foundZero = false;
+    row.forEach((num, numIndex) => {
+      if (num === 0) {
+        zeroColRecord[numIndex] = 0;
+        foundZero = true;
+      }
+    })
+
+    if (foundZero) {
+      row[0] = 0;
+    }
+  });
+
+  matrix.forEach((row) => {
+    if(row[0] === 0) {
+      row.forEach((num, numIndex) => row[numIndex] = 0);
+    } else {
+      row.forEach((num, numIndex) => row[numIndex] = zeroColRecord[numIndex]);
+    }
+  });
+
+  return matrix;
+}
