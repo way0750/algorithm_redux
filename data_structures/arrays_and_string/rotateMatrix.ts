058348444f6e86ba038309f37345eb4f4920cbd8
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-06 00:47:21 -0700

    add spec and passing

--- data_structures/arrays_and_string/rotateMatrix.ts
+++ data_structures/arrays_and_string/rotateMatrix.ts
@@ -37,3 +37,36 @@ export function roateMatrix(matrix) {
   matrix = flipDownwardDiaganal(matrix);
   return matrix;
 }
+
+describe('rotate matrix', () => {
+  it('should return correct 001', () => {
+    const matrix = [
+      [1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10,11,12],
+      [13,14,15,16]
+    ];
+    const expected = [
+      [13, 9, 5, 1],
+      [14, 10, 6, 2],
+      [15, 11, 7, 3],
+      [16, 12, 8, 4]
+    ];
+
+    expect(roateMatrix(matrix)).to.deep.equal(expected);
+  });
+  it('should return correct 002', () => {
+    const matrix = [
+      [1, 2, 3],
+      [5, 6, 7],
+      [9, 10,11],
+    ];
+    const expected = [
+      [9, 5, 1],
+      [10, 6, 2],
+      [11, 7, 3]
+    ];
+
+    expect(roateMatrix(matrix)).to.deep.equal(expected);
+  });
+});
