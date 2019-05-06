9ff0cae4d32401935e469d1aa1857f161bd029a5
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-05 23:21:56 -0700

    add solution

--- data_structures/arrays_and_string/palindromePermutation.ts
+++ data_structures/arrays_and_string/palindromePermutation.ts
@@ -5,4 +5,20 @@
   EXAMPLE
   Input: Tact Coa
   Output: True (permutations: "taco cat". "atco cta". etc.)
+
+  solution 1:
+ * for a string to be rearranged into a palindrome, you just need to make sure
+ * that the amount of characters that have odd count is 1 or 0
+ * so set a oddCharCount and initialized to 0
+ * set a record to {}
+ * then go through each char and increase its count in record first
+ * then check its frequency count
+ *   if it is even then reduce oddCharCount by 1
+ *   else increase it by 1
+ * 
+ * time and space
+ * time: you have to go through each character in the string so n for the string
+ * length
+ * 
+ * space: worse case you end up with all unique chars so..... n again
  */
