13a9038bb0702b15d953b5eecbf6f7fe3830901c
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-05 23:45:36 -0700

    add solution

--- data_structures/arrays_and_string/oneAway.ts
+++ data_structures/arrays_and_string/oneAway.ts
@@ -4,4 +4,13 @@
  * Given two strings, write a function to check if they are one edit (or zero edits) away.
   EXAMPLE
   pale, pIe -> true pales. pale -> true pale. bale -> true pale. bake -> false
+
+  solution 1:
+ * if the difference between two string is more than 1, that means it will be
+ * more than 1 edit, return false;
+ * if same length, then check character at each index, and maintain a count of
+ * how many different character you have found, if more than 1, then return false;
+ * if different length (by 1) then you loop through the longer one to compare
+ * curent long str char to current short str char, if different then allow the
+ * short str index to stay once. if it has already stayed once then return false
  */
