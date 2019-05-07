1b05ed8df536968bd2bf9a481d437588a687d4b6
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-06 22:58:59 -0700

    add simple linked list implementation

---
+++ data_structures/linkedList/linkedLIst.ts
@@ -0,0 +1,52 @@
+/**
+ * singles and doubly linked lists classes
+ */
+
+export class Node {
+  public value: any;
+  public next: any;
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+export class LinkedList {
+  public head: null | Node;
+  public tail: null | Node;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  public appendToTail(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else if (!this.tail) {
+      this.head.next = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+}
+
+describe('Single Linked List', () => {
+  it('should work with one node', () => {
+    const newList = new LinkedList();
+    newList.appendToTail(11);
+    expect(newList.head.value).to.eql(11);
+    expect(newList.head.next).to.eql(null);
+    expect(newList.tail).to.eql(null);
+  });
+  it('should work with two node', () => {
+    const newList = new LinkedList();
+    newList.appendToTail(11);
+    newList.appendToTail(22);
+    expect(newList.head.value).to.eql(11);
+    expect(newList.head.next.value).to.eql(22);
+    expect(newList.tail.value).to.eql(22);
+  });
+});
