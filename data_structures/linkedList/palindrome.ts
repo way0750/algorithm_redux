bf50119108b0e9228a4b606b5f29243b18fc41b2
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-07 02:26:52 -0700

    add spec

--- data_structures/linkedList/palindrome.ts
+++ data_structures/linkedList/palindrome.ts
@@ -10,8 +10,8 @@
  * 
  * set global boolean: isPalindrome to true;
  * base case:
- *   if B is null, then it is an even length list
- * then check A.value === B.value, if isPalindrome is true
+ *   if B.next is null, then it is an even length list
+ * then check A.value === A.next.value, if isPalindrome is true
  * and A.value !== A.next.value then set isPalindrome to false
  * else return A.next.next
  *   if B.next is null, then it is an odd length list
@@ -28,3 +28,83 @@
  *   of the base case setup above ^^^
  */
 
+import { LinkedList, Node } from './linkedList';
+
+
+export function isPalindromeList(list): boolean {
+  let isPalindrome = true;
+  function search(nodeA: Node, nodeB: Node ) {
+    // even length list
+    if(!nodeB.next) {
+      if (nodeA.value !== nodeA.next.value) {
+        isPalindrome = false;
+      }
+      return nodeA.next.next;
+    } else if (!nodeB.next.next) {
+      // odd length list
+      if (nodeA.value !== nodeA.next.next.value) {
+        isPalindrome = false;
+      }
+      return nodeA.next.next.next;
+    }
+
+    const leftSideNode = search(nodeA.next, nodeB.next.next);
+    if (isPalindrome) {
+      isPalindrome = nodeA.value === leftSideNode.value;
+    }
+
+    return leftSideNode.next;
+  }
+
+  const nodeA = list.head;
+  const nodeB = list.head ? list.head.next : null;
+  // if the list has 0 or 1 node then just return true
+  if (!nodeB) {
+    return true;
+  } else {
+    search(nodeA, nodeB);
+    return isPalindrome;
+  }
+}
+
+describe('Palindrome', () => {
+  it('should return true for aabbaa', () => {
+    const list = new LinkedList();
+    list.appendToTail('a');
+    list.appendToTail('a');
+    list.appendToTail('b');
+    list.appendToTail('b');
+    list.appendToTail('a');
+    list.appendToTail('a');
+    expect(isPalindromeList(list)).to.be.true;
+  });
+  it('should return false for aabcdbaa', () => {
+    const list = new LinkedList();
+    list.appendToTail('a');
+    list.appendToTail('a');
+    list.appendToTail('b');
+    list.appendToTail('c');
+    list.appendToTail('d');
+    list.appendToTail('b');
+    list.appendToTail('a');
+    list.appendToTail('a');
+    expect(isPalindromeList(list)).to.be.false;
+  });
+  it('should return true for aabcbaa', () => {
+    const list = new LinkedList();
+    list.appendToTail('a');
+    list.appendToTail('a');
+    list.appendToTail('b');
+    list.appendToTail('c');
+    list.appendToTail('b');
+    list.appendToTail('a');
+    list.appendToTail('a');
+    expect(isPalindromeList(list)).to.be.true;
+  });
+  it('should return false for ab', () => {
+    const list = new LinkedList();
+    list.appendToTail('a');
+    list.appendToTail('b');
+    expect(isPalindromeList(list)).to.be.false;
+  });
+});
