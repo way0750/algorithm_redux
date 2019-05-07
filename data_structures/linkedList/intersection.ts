cf619e456903a387895ba6161a1ef681c529210b
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-07 02:41:30 -0700

    add implementation

--- data_structures/linkedList/intersection.ts
+++ data_structures/linkedList/intersection.ts
@@ -1,3 +1,5 @@
+import { LinkedList } from "./linkedLIst";
+
 /**
  * Intersection: Given two (singly) linked lists, determine if the two lists intersect.
  * Return the intersecting node. Note that the intersection is defined based on
@@ -14,3 +16,24 @@
  * space you are adding new property to all the node worse
  * case it longest list * 2
  */
+
+export function isIntersection(list1: LinkedList, list2: LinkedList): boolean {
+  let nodeA = (list1.head as any);
+  let nodeB = (list2.head as any);
+  if (!nodeA || !nodeB) {
+    return false;
+  }
+  while (nodeA || nodeB) {
+    nodeA = nodeA || {};
+    nodeB = nodeB || {};
+    if(nodeA.isVisited || nodeB.isVisited) {
+      return true;
+    }
+    nodeA.isVisited = true;
+    nodeA = nodeA.next;
+
+    nodeB.isVisited = true;
+    nodeB = nodeB.next;
+  }
+  return false;
+}
