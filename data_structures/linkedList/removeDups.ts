7dc3105ab0917239f0a8164846b4b06eac9bad62
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-07 00:30:09 -0700

    add more solution

--- data_structures/linkedList/removeDups.ts
+++ data_structures/linkedList/removeDups.ts
@@ -8,6 +8,16 @@
  * set preNode to null
  * set curNode to linkedList's head
  * as long as curNode exists keep looping
+ * 
+ * time and space:
+ * time: loop through once so n
+ * space: worst case you end up with all unique values, so n again
+ * 
+ * solution 2: not allow to use buffer to keep frequency record:
+ * compare all subsequent value to current node and remove the duplicated ones
+ * time and space:
+ * time: n**2
+ * space: you are not using extra space so constant
  */
 
 import { LinkedList } from './linkedLIst';
@@ -36,13 +46,33 @@ export function removeDupBufferOk(linkedList): LinkedList {
   return linkedList;
 }
 
-describe('remove dups', () => {
+export function removeDupNoBuffer(list): LinkedList {
+  let currentNode = list.head;
+  while(currentNode) {
+    let previousNode = currentNode;
+    let runner = currentNode.next;
+    while(runner) {
+      if(runner.value === currentNode.value) {
+        previousNode.next = runner.next;
+      } else {
+        previousNode = runner;
+      }
+      runner = runner.next;
+    }
+
+    currentNode = currentNode.next;
+  }
+
+  return list;
+}
+
+describe('remove dups buffer ok', () => {
   it('should return 1, 2, 3', () => {
     const list = new LinkedList();
     list.appendToTail(1);
     list.appendToTail(1);
-    list.appendToTail(1);
     list.appendToTail(2);
+    list.appendToTail(1);
     list.appendToTail(2);
     list.appendToTail(3);
     const result = removeDupBufferOk(list);
@@ -53,6 +83,28 @@ describe('remove dups', () => {
     const list = new LinkedList();
     list.appendToTail(1);
     const result = removeDupBufferOk(list);
+    const values = result.mapToArray(({ value }) => value);
+    expect(values).to.eql([1]);
+  });
+});
+
+describe('remove dups no bugger', () => {
+  it('should return 1, 2, 3', () => {
+    const list = new LinkedList();
+    list.appendToTail(1);
+    list.appendToTail(1);
+    list.appendToTail(2);
+    list.appendToTail(1);
+    list.appendToTail(2);
+    list.appendToTail(3);
+    const result = removeDupNoBuffer(list);
+    const values = result.mapToArray(({ value }) => value);
+    expect(values).to.eql([1,2,3]);
+  });
+  it('should return 1', () => {
+    const list = new LinkedList();
+    list.appendToTail(1);
+    const result = removeDupNoBuffer(list);
     const values = result.mapToArray(({ value }) => value);
     expect(values).to.eql([1]);
   });
