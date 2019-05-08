3735bec706e1687d54885e29496cba839d36667e
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-08 00:59:23 -0700

    implemetation

--- data_structures/linkedList/LoopDetection.ts
+++ data_structures/linkedList/LoopDetection.ts
@@ -7,5 +7,36 @@
   Input: A -) B -) C -) 0 -) E -) C[thesameCasearlierl Output: C
 
     solution 1:
-    mark all the nodes
+    mark all nodes as you loop through one node at the time
+    anytime you found a already visited node, return that node
+
+    solution 2:
+    use the runner technique:
+ *  use two runner, one faster one slow, if any time the slow one is same as the
+ *  fast one, that means there a loop
  */
+
+export function isLoop(list) {
+  let slow = list.head;
+  let fast = list.head;
+  let keepLooping = true;
+  while(keepLooping && fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      keepLooping = false;
+    }
+  }
+
+  // collision found
+  if (!keepLooping) {
+    slow = list.head;
+    while(slow !== fast) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+    return slow;
+  } else {
+    return null;
+  }
+}
