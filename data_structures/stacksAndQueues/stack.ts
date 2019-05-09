a668711593acf6e700d5e9f149edb2ad2cb1ec2f
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-08 23:01:14 -0700

    add new challenge

---
+++ data_structures/stacksAndQueues/stack.ts
@@ -0,0 +1,10 @@
+/**
+ * implement a stack class without using array
+ * it has to have these instance methods:
+ * pop(), push(), peek(), isEmpty()
+ * 
+ * solution 1:
+ * use an plaint object, then use numbers as key, each time you add anything or
+ * remove anything, you use the property lastNumKey
+ * each time you add or remove you also update the current count
+ */
