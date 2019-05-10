8c5430fe58dbd4e830672cfa956aa915ae1784c2
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-10 00:22:34 -0700

    add solution

--- data_structures/stacksAndQueues/stackOfPlates.ts
+++ data_structures/stacksAndQueues/stackOfPlates.ts
@@ -6,5 +6,24 @@
  * a new stack once the previous one exceeds capacity. SetOfStacks. push () and SetOfStacks.
  * pop () should behave identically to a single stack (that is, pop ( ) should return
  * the same values as it would if there were just a single stack). FOLLOW UP Implement
- * a functionpopAt(int index) which performs a pop operation on a specific sub-stack.
+ * a function popAt(int index) which performs a pop operation on a specific sub-stack.
+ * 
+ * solution 1:
+ * use an Array to hold stacks, then each time you add, you check and see if the
+ * very last one can still accept new values, if yes, add to that stack, if no,
+ * then create new stack then add to that stack
+ * 
+ * for removing, get the target stack which is default to the very last one
+ * unless the user asks for specific stack by index.
+ * if a stack exists at that index, then remove from the top
+ * if that stack doesn't exist, then return undefined;
+ * 
+ * remember to update total length, and when you removing from a specific stack
+ * and it is already empty, don't update total length!
+ * 
+ * time and space
+ * 
+ * time: adding removing constant
+ * space: n as in the total amount of elements in the store
  */
+
