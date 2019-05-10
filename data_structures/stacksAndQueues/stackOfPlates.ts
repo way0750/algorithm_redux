d96d5ebb49c7baa899f239a0fa8d6c421af8a187
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-10 00:40:53 -0700

    add implementation

--- data_structures/stacksAndQueues/stackOfPlates.ts
+++ data_structures/stacksAndQueues/stackOfPlates.ts
@@ -1,3 +1,5 @@
+import { Stack } from "./stack";
+
 /**
  * Stack of Plates: Imagine a (literal) stack of plates. If the stack gets too high,
  * it might topple. Therefore, in real life, we would likely start a new stack when
@@ -18,12 +20,64 @@
  * if a stack exists at that index, then remove from the top
  * if that stack doesn't exist, then return undefined;
  * 
- * remember to update total length, and when you removing from a specific stack
- * and it is already empty, don't update total length!
- * 
  * time and space
  * 
  * time: adding removing constant
  * space: n as in the total amount of elements in the store
  */
 
+export class SetOfStacks {
+  // to keep track of length of all elements, but will have a method to return
+  // this so user can't mutate it
+  private totalLength: number = 0;
+  private store: Array<Stack> = [new Stack()];
+  private stackLimit: number = 0;
+  constructor(stackLimit = 5) {
+    this.stackLimit = stackLimit;
+  }
+
+  public push (value) {
+    let lastStack = this.store[this.store.length - 1];
+    if(lastStack.length() < this.stackLimit) {
+      lastStack.push(value);
+    } else {
+      lastStack = new Stack();
+      lastStack.push(value);
+      this.store.push(lastStack);
+    }
+
+    this.totalLength++;
+  }
+
+  public length() {
+    return this.totalLength;
+  }
+
+  public popAt(stackId = this.store.length - 1) {
+    // stackId has to be within the valid interval
+    // from 0...store length(exclusive)
+    if (stackId >= this.store.length || stackId < 0) {
+      return undefined;
+    }
+
+    const targetStack = this.store[stackId];
+    if (targetStack.isEmpty()) {
+      this.store.splice(stackId, 1);
+      return undefined;
+    } else {
+      const poppedVal = targetStack.pop();
+      this.totalLength--;
+      // to handle the last stack being empty after popping
+      // so if right this round popping, if the user wants to pop again
+      // we will pop from 2nd to last stack
+      if (targetStack.isEmpty) {
+        this.store.splice(stackId, 1);
+      }
+      return poppedVal;
+    }
+  }
+
+  public pop() {
+    return this.popAt(this.store.length - 1);
+  }
+}
