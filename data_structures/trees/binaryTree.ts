7bdf6f88fda4ab0747e0db4a7208c9913ee33c7d
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-11 03:04:37 -0700

    add binary search tree

--- data_structures/trees/binaryTree.ts
+++ data_structures/trees/binaryTree.ts
@@ -18,12 +18,10 @@ export class GenericTree {
 export class BinaryTree extends GenericTree {
   public leftChild: GenericTree = this.children[0];
   public rightChild: GenericTree = this.children[1];
-  public isSearchTree: Boolean = false;
   public isBinaryTree: Boolean = true;
 
-  constructor(value, { isSearchTree }) {
+  constructor(value) {
     super(value);
-    this.isBinaryTree = isSearchTree;
   }
 
   public addLeftChild(node) {
@@ -35,6 +33,10 @@ export class BinaryTree extends GenericTree {
     this.rightChild = node;
     this.children[1] = node;
   }
+}
+
+export class BinarySearchTree extends BinaryTree {
+  public isSearchTree: Boolean = true;
 }
 
 /**
@@ -56,31 +58,62 @@ const ORDERS = {
 };
 
 export function toArray(tree, order = ORDERS.PRE_ORDER) {
+  const arr = [];
+  treeTraversal(tree, (node) => arr.push(node.value), order);
+  return arr;
+}
+
+// export function toArrayOld(tree, order = ORDERS.PRE_ORDER) {
+//   if (!tree) {
+//     return [];
+//   }
+
+//   // only binary trees can do in order traversal
+//   if (order === ORDERS.IN_ORDER && tree.isBinaryTree) {
+//     const leftSubArray = toArrayOld(tree.leftChild, order);
+//     const rightSubArray = toArrayOld(tree.rightChild, order);
+//     return [...leftSubArray, tree.value, ...rightSubArray] ;
+//   }
+  
+//   const array = [];
+//   const allSubArray = tree.children.reduce((array, childNode) => {
+//     const subArray = toArrayOld(childNode, order);
+//     array.push(...subArray);
+//     return array;
+//   }, []);
+
+//   if (order === ORDERS.PRE_ORDER) {
+//     array.push(tree.value, ...allSubArray);
+//   } else if (order === ORDERS.POST_ORDER) {
+//     array.push(...allSubArray, tree.value);
+//   }
+
+//   return array;
+// }
+
+export function treeTraversal(tree, callBack = (...args) => {}, order = ORDERS.PRE_ORDER) {
   if (!tree) {
-    return [];
+    return;
   }
 
   // only binary trees can do in order traversal
   if (order === ORDERS.IN_ORDER && tree.isBinaryTree) {
-    const leftSubArray = toArray(tree.leftChild, order);
-    const rightSubArray = toArray(tree.rightChild, order);
-    return [...leftSubArray, tree.value, ...rightSubArray] ;
+    treeTraversal(tree.leftChild, callBack, order);
+    callBack(tree);
+    treeTraversal(tree.rightChild, callBack, order);
   }
-  
-  const array = [];
-  const allSubArray = tree.children.reduce((array, childNode) => {
-    const subArray = toArray(childNode, order);
-    array.push(...subArray);
-    return array;
-  }, []);
 
   if (order === ORDERS.PRE_ORDER) {
-    array.push(tree.value, ...allSubArray);
+    callBack(tree);
+    tree.children.forEach((childNode) => {
+      treeTraversal(childNode, callBack, order);
+    });
   } else if (order === ORDERS.POST_ORDER) {
-    array.push(...allSubArray, tree.value);
+    tree.children.forEach((childNode) => {
+      treeTraversal(childNode, callBack, order);
+    });
+    callBack(tree);
   }
-
-  return array;
 }
 
 /**
@@ -98,28 +131,16 @@ export function depthFirstTreeSearch(tree, callBack) {
   });
 }
 
-export function depthFirstTreeTraversal(tree, callBack) {
-  const keepLooping = (node) => {
-    callBack(node);
-    return false;
-  };
-
-  depthFirstTreeSearch(tree, keepLooping);
-}
-
 export function nodeCount(tree) {
-  if (!tree) {
-    return 0;
-  } else {
-    return tree.children.reduce((sum, childNode) => {
-      return sum + nodeCount(childNode);
-    }, 1);
-  }
+  let count = 0;
+  treeTraversal(tree, () => ++count);
+  return count;
 }
 
 describe('Tree', () => {
   describe('Tree', () => {
-    it('Should be able to create a tree', () => {
+    let topTreeNode;
+    beforeEach(() => {
       const tree1 = new GenericTree(1);
       const tree2 = new GenericTree(2);
       const tree3 = new GenericTree(3);
@@ -135,62 +156,85 @@ describe('Tree', () => {
       tree2.children.push(tree5);
       tree5.children.push(tree6, tree7);
       tree7.children.push(tree8, tree9, tree10);
-      const count = nodeCount(tree1);
+      topTreeNode = tree1;
+    });
+    it('Should be able to create a tree', () => {
+      const count = nodeCount(topTreeNode);
       expect(count).to.eql(10);
     });
     it('Should be able to convert a tree into an array', () => {
-      const tree1 = new GenericTree(1);
-      const tree2 = new GenericTree(2);
-      const tree3 = new GenericTree(3);
-      const tree4 = new GenericTree(4);
-      const tree5 = new GenericTree(5);
-      const tree6 = new GenericTree(6);
-      const tree7 = new GenericTree(7);
-      const tree8 = new GenericTree(8);
-      const tree9 = new GenericTree(9);
-      const tree10 = new GenericTree(10);
 
-      tree1.children.push(tree2, tree3, tree4);
-      tree2.children.push(tree5);
-      tree5.children.push(tree6, tree7);
-      tree7.children.push(tree8, tree9, tree10);
       // pre order
-      let valArr = toArray(tree1);
+      let valArr = toArray(topTreeNode);
       expect(valArr).to.eql([1,2,5,6,7,8,9,10,3,4]);
+      valArr = [];
+      treeTraversal(topTreeNode, (tree) => {
+        valArr.push(tree.value);
+      }, ORDERS.PRE_ORDER);
+      expect(valArr).to.eql([1,2,5,6,7,8,9,10,3,4]);
+
       // default to pre order
-      valArr = toArray(tree1);
+      valArr = toArray(topTreeNode);
       expect(valArr).to.eql([1,2,5,6,7,8,9,10,3,4]);
 
       // post order
-      valArr = toArray(tree1, ORDERS.POST_ORDER);
+      valArr = toArray(topTreeNode, ORDERS.POST_ORDER);
       expect(valArr).to.eql([6,8,9,10,7,5,2,3,4,1]);
     });
     it('Should be able to traversal through a tree', () => {
-      const tree1 = new GenericTree(1);
-      const tree2 = new GenericTree(2);
-      const tree3 = new GenericTree(3);
-      const tree4 = new GenericTree(4);
-      const tree5 = new GenericTree(5);
-      const tree6 = new GenericTree(6);
-      const tree7 = new GenericTree(7);
-      const tree8 = new GenericTree(8);
-      const tree9 = new GenericTree(9);
-      const tree10 = new GenericTree(10);
-
-      tree1.children.push(tree2, tree3, tree4);
-      tree2.children.push(tree5);
-      tree5.children.push(tree6, tree7);
-      tree7.children.push(tree8, tree9, tree10);
       const values = [];
-      depthFirstTreeTraversal(tree1, (node) => {
+      treeTraversal(topTreeNode, (node) => {
         values.push(node.value);
       });
       expect(values).to.eql([1,2,5,6,7,8,9,10,3,4]);
-      expect(depthFirstTreeSearch(tree1, (node) => node.value === 10 )).to.be.true;
+      expect(depthFirstTreeSearch(topTreeNode, (node) => node.value === 10 )).to.be.true;
+      expect(depthFirstTreeSearch(topTreeNode, (node) => node.value === 11 )).to.be.false;
     });
   });
-  describe('Binary Tree', () => {
-  });
-  describe('Binary Tree', () => {
+
+  describe('Binary Search Tree', () => {
+    let topTreeNode;
+    beforeEach(() => {
+      const tree1 = new BinarySearchTree(1);
+      const tree2 = new BinarySearchTree(2);
+      const tree3 = new BinarySearchTree(3);
+      const tree4 = new BinarySearchTree(4);
+      const tree5 = new BinarySearchTree(5);
+      const tree6 = new BinarySearchTree(6);
+      const tree7 = new BinarySearchTree(7);
+      const tree8 = new BinarySearchTree(8);
+      const tree9 = new BinarySearchTree(9);
+      const tree10 = new BinarySearchTree(10);
+
+      tree4.addLeftChild(tree2);
+      tree2.addLeftChild(tree1);
+      tree2.addRightChild(tree3);
+      tree4.addRightChild(tree7);
+      tree7.addLeftChild(tree6);
+      tree6.addLeftChild(tree5);
+      tree7.addRightChild(tree9);
+      tree9.addLeftChild(tree8);
+      tree9.addRightChild(tree10);
+      topTreeNode = tree4;
+    });
+
+    it('Should be able to create a Binary Search tree', () => {
+      const count = nodeCount(topTreeNode);
+      expect(count).to.eql(10);
+      const values = toArray(topTreeNode, ORDERS.IN_ORDER);
+      expect(values).to.eql([1,2,3,4,5,6,7,8,9,10]);
+    });
+
+    it('Should be able to go through a Binary Search tree', () => {
+      const values = [];
+      treeTraversal(topTreeNode, (node) => {
+        values.push(node.value);
+      }, ORDERS.IN_ORDER);
+      expect(values).to.eql([1,2,3,4,5,6,7,8,9,10]);
+      let found = depthFirstTreeSearch(topTreeNode, (node) => node.value === 7);
+      expect(found).to.be.true;
+      found = depthFirstTreeSearch(topTreeNode, (node) => node.value === 77);
+      expect(found).to.be.false;
+    });
   });
 })
