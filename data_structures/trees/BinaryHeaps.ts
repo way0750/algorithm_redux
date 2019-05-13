4a5a6de5937a0da493baece1c73e15e15d3fc142
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-12 17:48:21 -0700

    add new challenge

---
+++ data_structures/trees/BinaryHeaps.ts
@@ -0,0 +1,12 @@
+/**
+ * Binary Heaps min and max heap
+ * required methods:
+ * Because we will use an array to implement a the heap because it will a
+ * complete tree
+ * so we will need:
+ * find parent index given child index
+ * find child index given parent index
+ * 
+ * add value and sort
+ * remove/extra min/max and resort
+ */
