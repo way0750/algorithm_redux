de1757d49babd06882a2bec236d463011bee7c32
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-14 01:54:23 -0700

    add solution

--- data_structures/trees/minimalTree.ts
+++ data_structures/trees/minimalTree.ts
@@ -1,4 +1,19 @@
 /**
  * Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
  * 
+ * solution 1:
+ * recursively do this:
+ * input an array, take the middle node, the recursively call the remaining left
+ * side of the array, and assign the return as the left child, do the same with
+ * right side of remaining array
+ * 
+ * base case: input is an empty array return null for leaf
+ * what to always return: a node or a null pointer
+ * what to do with returns:
+ *   if it is from left, then assign as left child
+ *   if it is from right, then assign as right child
+ * how to make problem smaller:
+ *   take center value and make it the center node
+ *   call left side of the array
+ *   call right side of the array
  */
