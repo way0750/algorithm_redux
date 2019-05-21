64af4ef52c94a1c47407133108f1231647d2c69e
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-20 23:55:18 -0700

    add new challenge

--- data_structures/trees/RandomNode.ts
+++ data_structures/trees/RandomNode.ts
@@ -4,4 +4,6 @@
  * returns a random node from the tree. All nodes should be equally likely
  * to be chosen. Design and implement an algorithm for getRandomNode, and
  * explain how you would implement the rest of the methods.
+ * 
+ * 
  */
