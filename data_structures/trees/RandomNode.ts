4e24370d441e0e604867a2502568e4aa80da8869
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-21 01:07:01 -0700

    temp save

--- data_structures/trees/RandomNode.ts
+++ data_structures/trees/RandomNode.ts
@@ -5,5 +5,71 @@
  * to be chosen. Design and implement an algorithm for getRandomNode, and
  * explain how you would implement the rest of the methods.
  * 
- * 
+ * insert just recursively insert new value, but of course, if value should go
+ * left but there isn't a left child, then just simply make it the left child
  */
+
+class TreeNode {
+  public leftChild?: TreeNode;
+  public rightChild?: TreeNode;
+  public parent?: TreeNode;
+  public value?: number;
+  constructor(value) {
+    this.value = value;
+  }
+
+  public insert(newNode: TreeNode) {
+    if (newNode.value > this.value) {
+      if (!this.rightChild) {
+        this.rightChild = newNode;
+      } else {
+        this.rightChild.insert(newNode);
+      }
+    } else {
+      if (!this.leftChild) {
+        this.leftChild = newNode;
+      } else {
+        this.leftChild.insert(newNode);
+      }
+    }
+  }
+}
+
+export class BinaryTree {
+  // this will be helpful to get random node
+  public length = 0;
+  public treeTop = null;
+
+  // let's keep things simple and assume all values are just numbers
+  public insert(value: number | TreeNode) {
+    const newNode = typeof value === 'number' ? new TreeNode(value) : value;
+    if(!this.treeTop) {
+      newNode.parent = null;
+      this.treeTop = newNode;
+    } else {
+      newNode.parent = this;
+      this.treeTop.insert(newNode);
+    }
+  }
+
+  // provide a node reference and delete if from the tree
+  public deleteNode(node: TreeNode) {
+    if (node.leftChild && node.rightChild) {
+      const replacementNode = this.getFurthestLeftNodeOnRight(node.rightChild);
+    } else if (!node.leftChild) {
+
+    } else if (!node.rightChild) {
+
+    }
+  }
+
+  private getFurthestLeftNodeOnRight(node) {
+    let leftMost = node;
+    while (node) {
+      leftMost = node
+      node = node.leftChild;
+    }
+
+    return leftMost;
+  }
+}
