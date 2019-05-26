26ea9b1cbb6c4806c93ea5018dfa996ff16ebcfa
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-25 21:43:53 -0700

    add sinon

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,4 @@
 // this is available in the global scope because this index.d.ts is at root level
 // TODO: should extend any globally available librarys' types to global
 declare const expect: Chai.ExpectStatic;
+declare const sinon: Chai.ExpectStatic;
--- testing/bootstrap.js
+++ testing/bootstrap.js
@@ -1,2 +1,4 @@
 const chai = require('chai');
+var sinon = require('sinon');
 Object.assign(global, chai);
+Object.assign(global, sinon);
