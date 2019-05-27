aac3659bf693df03bb364dca9f14507891fa4441
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-26 22:09:39 -0700

    add solution

--- data_structures/trees/pathWithSum.ts
+++ data_structures/trees/pathWithSum.ts
@@ -4,4 +4,58 @@
  * Design an algorithm to count the number of paths that sum to a given
  * value. The path does not need to start or end at the root or a
  * leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+ * 
+ * solution 1:
+ * each path in the tree, if you flatten it into an array, it's easier to solve this
+ * k = 4
+ * [10, 7, -17, 17, -17, 17, -17, 21, -21, 4, 0]
+ * the basic idea:
+ * 
+ * all the running/accumulative sum:
+ *      if the latest running sum - k result in a previous sum, that
+ *      means a sub section can be summed up to k
+ * ex: 
+ *      |   |  <--- the difference 
+ * -----
+ * ---------
+ * 
+ * but of course there can be multiple running that are the same
+ * so for this array
+ * [10, 7, -17, 17, -17, 17, -17, 21, -21, 4, 0]
+ * you have this running sum
+ * [10, 17,  0, 17,   0, 17,   0, 21,   0, 4, 4]
+ * turn this into a hash to make it easier to find the amount of sub array
+ * to sum up to k:
+ * {
+ *   10: 1,
+ *   17: 3,
+ *   0: 4 + 1 (initialize it to 1 to deal with running sum that is the same as k)
+ *   4: 2
+ *   21: 1
+ * }
+ * 
+ * so if running sum is 4 (first running sum, 4), and k = 4
+ * so 4 - 4 === 0
+ * use that 0 to find in the hash, how many sub array could be summed up to
+ * current value to sum to k
+ * in this case it is 5,
+ * 
+ * 
+ * steps:
+ * get running sum: previous sum + current value, that is the new running sum
+ *   add +1 to hash[sum]
+ * then current sum -  current number
+ *   that's the key, use it to find total amount of sub arrays can sum up to k
+ *   and end at current ele
+ * then recursively keep calling with this hash
+ * but when coming back up and up, -1 of current sum, you can't reuse it anymore
+ * 
+ * recursion:
+ * base case:
+ *   if node is null, return 0, that is the amount of sub array can be summed to k
+ * what to return always: number, that is the total amount
+ * what to do with returns: add them to current total, so current total +
+ *   children total
+ *   then before returning, -1 for current running sum
+ * how to make problem smaller: recursively call left and right
  */
