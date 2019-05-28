c53d285fa25da39c77a742cbd377809bd18a6860
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-27 23:26:01 -0700

    add solution

--- algorithms_2/dynamic_programming/allSets.ts
+++ algorithms_2/dynamic_programming/allSets.ts
@@ -1,3 +1,18 @@
 /**
  * return all subsets of a set.
+ * 
+ * solution 1
+ * just initialize a set of one empty set [[]], cal it main set
+ * then loop through input set, add current num to each and every one of the
+ * main set (make new set, don't mutate the main set yet)
+ * then add those new ones back into the main set
+ * 
+ * return main set;
+ * 
+ * time and space
+ * time, you will be doubling the amount of sets in the main set
+ * you can see that as a binary tree, and you are trying to get node count,
+ * so branch**(depth + 1)
+ * 2**n+1 or 2**n
+ * space: simply 2**n
  */
