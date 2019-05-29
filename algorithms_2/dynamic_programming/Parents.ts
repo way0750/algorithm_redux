cb0d43faebd72f08377b63b9dba80e3434bfcc68
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-29 00:46:52 -0700

    add implementation

--- algorithms_2/dynamic_programming/Parents.ts
+++ algorithms_2/dynamic_programming/Parents.ts
@@ -4,4 +4,37 @@
  * EXAMPLE
  * Input: 3
  * Output: ((())), (())(), (()()), ()(()), ()()()
+ * 
+ * solution 1:
+ * you can go from 1 ... to n because you can reuse previous results
+ * ex 1: ()
+ * then for 2, you can add a new one to by adding the ( to where it wouldn't
+ * break any parent, and then add ) to the end
+ * so (),
+ * (())     (()   ()()
+ * ^ yes     ^ no   ^ yes
  */
+
+export function getParens(count) {
+  let finalParens = [];
+  while (count > 0) {
+    count--;
+    finalParens = finalParens.reduce((curParens, pattern) => {
+      let openParenCount = 0;
+      for (let i = 0; i <= pattern.length; i++) {
+        if (!openParenCount) {
+          const leftStr = pattern.slice(0, i);
+          const rightStr = pattern.slice(i);
+          curParens.push(`${leftStr}(${rightStr})`);
+        }
+        if (pattern[i] === '(') {
+          openParenCount++
+        } else if (pattern[i] === ')') {
+          openParenCount--;
+        }
+      }
+      return curParens;
+    }, []);
+  }
+  return finalParens;
+}
