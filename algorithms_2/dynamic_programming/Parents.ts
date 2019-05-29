f10e416f7194c786c43796aefabde3716babb81a
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-29 00:51:03 -0700

    add spec and passing

--- algorithms_2/dynamic_programming/Parents.ts
+++ algorithms_2/dynamic_programming/Parents.ts
@@ -15,8 +15,8 @@
  * ^ yes     ^ no   ^ yes
  */
 
-export function getParens(count) {
-  let finalParens = [];
+export function getParens(count = 0) {
+  let finalParens = [''];
   while (count > 0) {
     count--;
     finalParens = finalParens.reduce((curParens, pattern) => {
@@ -38,3 +38,22 @@ export function getParens(count) {
   }
   return finalParens;
 }
+
+describe('Parens', () => {
+  it('should return array of one empty string for input 0', () => {
+    const count = 0;
+    expect(getParens(count)).to.eql(['']);
+  });
+  it('should return array of single paren for input 1', () => {
+    const count = 1;
+    expect(getParens(count)).to.eql(['()']);
+  });
+  it('should return array of 2 parens for input 2', () => {
+    const count = 2;
+    expect(getParens(count)).to.eql(['(())', '()()']);
+  });
+  it('should return array of 5 parens for input 3', () => {
+    const count = 3;
+    expect(getParens(count)).to.eql(['((()))', '(())()', '(()())', '()(())', '()()()']);
+  });
+});
