dbb15445602b37eee9da58ad42af8dd51e766d33
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-05-30 22:39:53 -0700

    add new solution

--- algorithms_2/dynamic_programming/coins.ts
+++ algorithms_2/dynamic_programming/coins.ts
@@ -2,4 +2,13 @@
  * Coins: Given an infinite number of quarters (25 cents), dimes (10 cents),
  * nickels (5 cents), and pennies (1 cent), write code to calculate the number
  * of ways of representing n cents.
+ * 
+ * solution 1, expensive recursion:
+ * go through each coin type and loop the amount of it from 0...until amount *
+ * coin type > input n
+ * then for each amount you do: amount * coin type then -n, and remaining.. you
+ * pass it to the next call without that coin type
+ * 
+ * 
+ * 
  */
