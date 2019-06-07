936860fd48ea23c69ed8062fa3abcb7cb829cd8a
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-06-07 09:29:05 -0700

    add new challenge

---
+++ algorithms_2/sortingRange.ts
@@ -0,0 +1,7 @@
+/**
+ * given an array of integers, find the smallest index range that you have to
+ * sort in order to sort the entire array
+ * ex: [1 3 2 4]
+ * return 1,2
+ * meaning: you just need to sort the range 1..2 (inclusive) to make this array sorted
+ */
