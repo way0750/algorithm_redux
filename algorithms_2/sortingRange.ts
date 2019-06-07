ba83bcf4447999501d5068ccffd477f18dcc28cc
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-06-07 09:51:41 -0700

    add spec and passing

--- algorithms_2/sortingRange.ts
+++ algorithms_2/sortingRange.ts
@@ -15,3 +15,30 @@
  * so n + nlogn, which could be simplified to nlogn
  * space: n for the sorted version of the input array
  */
+
+ export function smallestSortingRange(arr) {
+   const sorted = arr.slice().sort((v1, v2) => v1 >= v2);
+   let FI = 0;
+   let BI = arr.length - 1;
+   while ((arr[FI] === sorted[FI] || arr[BI] === sorted[BI]) && BI > FI) {
+     if (arr[FI] === sorted[FI]) {
+       FI++;
+     }
+     if (arr[BI] === sorted[BI]) {
+       BI--
+     }
+   }
+
+   return FI >= BI ? [] : [FI, BI];
+ }
+
+ describe('Sorting Range', () => {
+   it('Should work example', () => {
+    const arr = [1, 3, 2, 4];
+    expect(smallestSortingRange(arr)).to.eql([1,2]);
+   });
+   it('Should return empty array for no range found', () => {
+    const arr = [1,2,3,4,5];
+    expect(smallestSortingRange(arr)).to.eql([]);
+   });
+ });
