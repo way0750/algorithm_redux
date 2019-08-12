ae586ba2f63ba1f3e1b87fd79aad26404b9368bb
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-08-11 23:39:12 -0700

    add implementation and spec

---
+++ algorithms_2/binarySearch.ts
@@ -0,0 +1,42 @@
+/**
+ * noting but the good old binary search..
+ * sorted array of integers, give a targe, find it
+ */
+
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+  while(end>start) {
+    let mid = start + Math.floor((end-start)/2);
+    const midNum = arr[mid];
+    if (midNum === target ) {
+      return true;
+    } else if (midNum < target) {
+      // move the search range to the right
+      start = mid + 1;
+    } else {
+      // mid > target
+      end = mid - 1;
+    }
+  }
+
+  return false;
+}
+
+describe('Binary Search', () => {
+  it('should return correctly for emtpy array target: 4', () => {
+    const arr = [];
+    const target  = 4;
+    expect(binarySearch(arr, target)).to.false;
+  });
+  it('should return correctly for 1,2,3,4,5,6,7,8 target: 4', () => {
+    const arr = [1,2,3,4,5,6,7,8];
+    const target  = 4;
+    expect(binarySearch(arr, target)).to.true;
+  });
+  it('should return correctly for 1,2,3,4,5,6,7,8 target: 14', () => {
+    const arr = [1,2,3,4,5,6,7,8];
+    const target  = 14;
+    expect(binarySearch(arr, target)).to.false;
+  });
+});
