25553382557e0d35a5ed58763ec1d6dbe4ddf1b5
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-08-27 23:56:14 -0700

    add spec and passing

--- data_structures/trees/graph2.ts
+++ data_structures/trees/graph2.ts
@@ -21,6 +21,7 @@ export class Graph {
       // check if it === target 
       // if yes then return
       const nodeId = nodeIds.shift();
+      console.log(nodeId);
       if (nodeId === target) {
         return true;
       }
@@ -28,7 +29,7 @@ export class Graph {
       // visited===false, make them true, and then push into queue
       const edges = this.nodes[nodeId].edges;
       edges.forEach((edgeId) => {
-        if (this.nodes[edgeId].visited === false) {
+        if (!this.nodes[edgeId].visited) {
           this.nodes[edgeId].visited = true;
           nodeIds.push(edgeId);
         }
@@ -37,4 +38,72 @@ export class Graph {
 
     return false;
   }
+
+  breadthFirstSearchWithFragmentedGraph(target) {
+    /**
+     * in case tree is fragmented, we should go through the this.nodes
+     * and for each node, if it has yet been visited, then perform BFS on it
+     */
+    for (let nodeId in this.nodes) {
+      if (+nodeId === target) {
+        return true;
+      }
+      if (!this.nodes[nodeId].visited) {
+        if (this.breadthFirstSearch(nodeId, target)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
 }
+
+describe('Testing graph 2', () => {
+  it('001, should return true', () => {
+    const graph = new Graph();
+    const nodes = {
+      1: { edges: [2,3,4,6]},
+      2: { edges: [3,4,5]},
+      3: { edges: [4]},
+      4: { edges: [5,1,2]},
+      5: { edges: [2,3]},
+      6: { edges: [5,4,7]},
+      7: { edges: [6]},
+    };
+    Object.assign((graph as any).nodes, nodes);
+    expect(graph.breadthFirstSearch(1, 7)).to.be.true;
+  });
+  it('002, should return false', () => {
+    const graph = new Graph();
+    const nodes = {
+      1: { edges: [2,3,4]},
+      2: { edges: [3,4,5]},
+      3: { edges: [4,6]},
+      4: { edges: [5,1,2]},
+      5: { edges: [6,2,3]},
+      6: { edges: [5,4,7]},
+      7: { edges: []},
+    };
+    Object.assign((graph as any).nodes, nodes);
+    expect(graph.breadthFirstSearch(1, 8)).to.be.false;
+  });
+  it('003, should return true', () => {
+    const graph = new Graph();
+    const nodes = {
+      1: { edges: [2,3,4]},
+      2: { edges: [3,4,5]},
+      3: { edges: [4,6]},
+      4: { edges: [5,1,2]},
+      5: { edges: [6,2,3]},
+      6: { edges: [5,4]},
+      7: { edges: []},
+      8: { edges: []},
+      9: { edges: []},
+      10: { edges: []}
+    };
+    Object.assign((graph as any).nodes, nodes);
+    expect(graph.breadthFirstSearchWithFragmentedGraph(10)).to.be.true;
+    expect(graph.breadthFirstSearchWithFragmentedGraph(11)).to.be.false;
+  });
+});
