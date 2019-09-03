d49c7f87c7da318964c2b853b58ef95b160e42ba
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-09-02 23:45:21 -0700

    add spec and passing

--- data_structures/trees/graph_DFS.ts
+++ data_structures/trees/graph_DFS.ts
@@ -22,8 +22,45 @@ export function DFS(graph, node, callBack) {
   }
 
   graph[node].visited = true;
+  callBack(node);
   graph[node].edges.forEach((v) => {
     DFS(graph, v, callBack);
   });
 }
 
+function DFSForDisconnectedGraph(graph, node, callBack) {
+
+}
+
+describe('DFS for graph', () => {
+  it('should return 1,2,3,4', () => {
+    const graph = {
+      1: { edges: [2, 4, 5], visited: false},
+      2: { edges: [3, 4, 5, 1], visited: false},
+      3: { edges: [4, 6], visited: false},
+      4: { edges: [5, 1], visited: false},
+      5: { edges: [6, 1], visited: false},
+      6: { edges: [], visited: false},
+    };
+
+    const nums = [1,2,3,4,5,6];
+    const result = []
+    DFS(graph, 1, (n) => result.push(n))
+    expect(result).to.eql(nums);
+  });
+  it('should return 1,2,3,4', () => {
+    const graph = {
+      1: { edges: [2, 4, 5], visited: false},
+      2: { edges: [3, 4, 5, 1], visited: false},
+      3: { edges: [4, 6], visited: false},
+      4: { edges: [5, 1], visited: false},
+      5: { edges: [6, 1], visited: false},
+      6: { edges: [], visited: false},
+    };
+
+    const nums = [1,2,3,4,5,6];
+    const result = [];
+    DFS(graph, 1, (n) => result.push(n))
+    expect(result).to.eql(nums);
+  });
+});
