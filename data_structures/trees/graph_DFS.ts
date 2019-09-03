335ef8f70d43925463b083da1f0b176ca6c3854e
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-09-02 23:38:36 -0700

    add implementation

---
+++ data_structures/trees/graph_DFS.ts
@@ -0,0 +1,10 @@
+/**
+ * depth first search on graph
+ * 
+ * if using recursion:
+ * base case: node has been visited
+ * how to make problem smaller: go through each edges, if there isn't any
+ *   that's essentially another base case
+ * what to do with return: nothing
+ * what to always return: nothing
+ */
