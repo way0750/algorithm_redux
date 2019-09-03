8bad9fc55358091d52a5f1a7ae71fea544f0f732
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-09-02 23:50:59 -0700

    can now also deal with disconnected graph

--- data_structures/trees/graph_DFS.ts
+++ data_structures/trees/graph_DFS.ts
@@ -28,8 +28,10 @@ export function DFS(graph, node, callBack) {
   });
 }
 
-function DFSForDisconnectedGraph(graph, node, callBack) {
-
+function DFSForDisconnectedGraph(graph, callBack) {
+  for( let node in graph ) {
+    DFS(graph, +node, callBack);
+  }
 }
 
 describe('DFS for graph', () => {
@@ -50,17 +52,38 @@ describe('DFS for graph', () => {
   });
   it('should return 1,2,3,4', () => {
     const graph = {
+      1: { edges: [7, 2, 4, 5], visited: false},
+      2: { edges: [3, 4, 5, 1], visited: false},
+      3: { edges: [4, 6], visited: false},
+      4: { edges: [5, 1], visited: false},
+      5: { edges: [6, 1], visited: false},
+      6: { edges: [], visited: false},
+      7: { edges: [1], visited: false}
+    };
+
+    const nums = [1,7,2,3,4,5,6];
+    const result = [];
+    DFS(graph, 1, (n) => result.push(n))
+    expect(result).to.eql(nums);
+  });
+
+  it('should work with a disconnected graph', () => {
+    const graph = {
+      0: { edges: [], visited: false},
       1: { edges: [2, 4, 5], visited: false},
       2: { edges: [3, 4, 5, 1], visited: false},
       3: { edges: [4, 6], visited: false},
       4: { edges: [5, 1], visited: false},
       5: { edges: [6, 1], visited: false},
       6: { edges: [], visited: false},
+      7: { edges: [8], visited: false},
+      8: { edges: [9], visited: false},
+      9: { edges: [], visited: false}
     };
 
-    const nums = [1,2,3,4,5,6];
+    const nums = [0,1,2,3,4,5,6,7,8,9];
     const result = [];
-    DFS(graph, 1, (n) => result.push(n))
+    DFSForDisconnectedGraph(graph, (n) => result.push(n))
     expect(result).to.eql(nums);
   });
 });
