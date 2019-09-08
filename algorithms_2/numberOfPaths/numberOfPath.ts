2c6e4c5c65dff8ecb37e552a0542564a0045742c
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-09-07 21:13:50 -0700

    update description

--- algorithms_2/numberOfPaths/numberOfPath.ts
+++ algorithms_2/numberOfPaths/numberOfPath.ts
@@ -5,4 +5,16 @@
  * corner of the grid. Given n, the size of the grid’s axes, write
  * a function numOfPathsToDest that returns the number of the possible
  * paths the driverless car can take.
+ * 
+ * For convenience, let’s represent every square in the grid as a pair (i,j). The first coordinate in the pair denotes the east-to-west axis, and the second coordinate denotes the south-to-north axis. The initial state of the car is (0,0), and the destination is (n-1,n-1).
+
+The car must abide by the following two rules: it cannot cross the diagonal border. In other words, in every step the position (i,j) needs to maintain i >= j. See the illustration above for n = 5. In every step, it may go one square North (up), or one square East (right), but not both. E.g. if the car is at (3,1), it may go to (3,2) or (4,1).
+
+ * Explain the correctness of your function, and analyze its time and space
+ * complexities.
+
+ input:  n = 4
+ output: 5
+
+ 
  */
