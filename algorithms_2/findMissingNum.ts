ac6cc112dfbe63ec7190500a7c9cb509f536fb6b
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-09-17 00:30:53 -0700

    add solution, implementation, and spec

--- algorithms_2/findMissingNum.ts
+++ algorithms_2/findMissingNum.ts
@@ -13,3 +13,42 @@
  * at the immediate left and see if that the case too, if yes, then you still
  * have to keep going.
  */
+
+ // [1,2,3,4,6,7,8]
+ //  0 1 2 3 4 5 6
+export function FindFirstMissing(nums) {
+  let minIndex = 0;
+  let maxIndex = nums.length - 1;
+  while(minIndex <= maxIndex) {
+    const midIndex = minIndex + Math.floor((maxIndex - minIndex) / 2);
+    const midValue = nums[midIndex];
+
+    const leftIndex= midIndex - 1
+    const leftValue = nums[leftIndex];
+
+    if ((midValue - midIndex) > 1 && (leftValue - leftIndex) === 1) {
+      return midIndex + 1;
+    } else if ((midValue - midIndex) > 1 && (leftValue - leftIndex) > 1) {
+      maxIndex = midIndex - 1;
+    } else {
+      minIndex = midIndex + 1;
+    }
+  }
+
+  return nums.length + 1;
+}
+
+describe('Testing finding missing numbers', () => {
+  it('should work with the example: 1,2,3,4,6,7,8', () => {
+    const nums = [1,2,3,4,6,7,8];
+    expect(FindFirstMissing(nums)).to.eql(5);
+  });
+  it('should return 8 for 1,2,3,4,5,6,7', () => {
+    const nums = [1,2,3,4,5,6,7];
+    expect(FindFirstMissing(nums)).to.eql(8);
+  });
+  it('should return 1 for empty array', () => {
+    const nums = [];
+    expect(FindFirstMissing(nums)).to.eql(1);
+  });
+});
