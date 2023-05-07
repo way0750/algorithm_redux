a341521889adb3444e2fcc1453c36f77fb354240
Huiqiang Huang <bruce0750@gmail.com>
2023-05-08 02:49:20 +0700

    add implementation

--- algorithms_3/data_structures/string/space_replacer.ts
+++ algorithms_3/data_structures/string/space_replacer.ts
@@ -3,5 +3,17 @@
  * You may assume that the string has sufficient space at the end to hold the additional characters,
  * and that you are given the "true" length of the string. (Note: If implementing in Java,
  * please use a character array so that you can perform this operation in place.)
+ * 
+ * split string into an array of single chars
+ * map through the array and replace space with %20
+ * join mapped array
+ * 
+ * time: O(n), space: O(n*3) === O(n);
  */
 
+export function spaceReplace(str) {
+    const strArr = str.split('');
+    return strArr.map((char) => {
+        return char === ' ' ? '%20' : char;
+    }).join('');
+}
