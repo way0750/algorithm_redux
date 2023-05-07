e04c970128a99d6189c7b35774a6beb04a0dbea5
Huiqiang Huang <bruce0750@gmail.com>
2023-05-08 02:38:42 +0700

    add solution

--- algorithms_3/data_structures/string/checkPermut.ts
+++ algorithms_3/data_structures/string/checkPermut.ts
@@ -1,4 +1,18 @@
 /**
  * Check Permutation: Given two strings, write a method to decide
  * if one is a permutation of the other.
+ * 
+ * permutation: same chars but different order
+ * so check the existance of each char and its frequency
+ * 
+ * let freq be the cache keeping track of chars and their respective frequency
+ * loop through first string and add char and frequency to freq
+ * 
+ * then loop through the second str char by char to check if it exists
+ * and if yes, reduce the count by 1, if no short cut return false
+ * 
+ * if any time the 2 strings are not of the same length
+ * short cut return false because either there are more characters in one of the strings
+ * or there are unique chars
+ * so no need to keep on going checking anything
  */
