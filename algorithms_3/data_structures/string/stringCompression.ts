8f4b327921b0910b654d5001b6f4db2dc75c9de8
Huiqiang Huang <bruce0750@gmail.com>
2023-05-08 04:29:48 +0700

    add spec and passing

--- algorithms_3/data_structures/string/stringCompression.ts
+++ algorithms_3/data_structures/string/stringCompression.ts
@@ -4,4 +4,46 @@
  * "compressed" string would not become smaller than the original string, your method should
  * return the original string. You can assume the string has only uppercase and
  * lowercase letters (a - z).
+ * 
+ * let freq by an array of [{ char: string, freq: num }]
+ * loop through the string char by char
+ *  if current char is the same as the last one, freq++
+ *  if not, then add { char: current char, freq: 1 }
+ * and the end, map and join the freq
+ * return the shorter string
+ * 
+ * time: O(n), space: O(n);
  */
+
+export function stringCompression(str) {
+    if (str.length < 2) {
+        return str;
+    }
+    const freq = [{ char: str[0], freq: 0 }];
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        const lastCharFreq = freq[freq.length - 1];
+        if (lastCharFreq.char === char) {
+            lastCharFreq.freq++;
+        } else {
+            freq.push( { char, freq: 1 });
+        }
+    }
+
+    const compressedStr = freq.map(({ char, freq }) => {
+        return `${char}${freq}`;
+    }).join('');
+
+    return compressedStr.length < str.length ? compressedStr : str;
+}
+
+describe('string compression', () => {
+    it('should return correctly', () => {
+        const str = 'aabcccccaaa';
+        expect(stringCompression(str)).to.equal('a2b1c5a3');
+    });
+    it('should return the original string', () => {
+        const str = 'abcdefg';
+        expect(stringCompression(str)).to.equal('abcdefg');
+    });
+});
