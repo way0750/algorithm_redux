57e2238055122e0ff741370e8495c121dd90d332
Huiqiang Huang <bruce0750@gmail.com>
2023-05-09 06:10:02 +0700

    add new specs

--- algorithms_3/data_structures/linkedlist/sumList.ts
+++ algorithms_3/data_structures/linkedlist/sumList.ts
@@ -7,4 +7,77 @@
  * FOLLOW UP
  * Suppose the digits are stored in forward order. Repeat the above problem. EXAMPLE
  * Input: (6 -) 1 -) 7) + (2 -) 9 -) 5).Thatis,617 + 295. Output: 9 -) 1 -) 2.That is, 912.
+ * 
+ * keep on while looping through both lists (lengths might be different)
+ * set carryOver = 0;
+ * and do basic math at the each loop
+ *  if sum > 9 then so sum % 10
+ * create new list as you go
  */
+
+export function sumList (list1, list2) {
+    let curNode1 = list1;
+    let curNode2 = list2;
+    const newNodes = [];
+    let carryOver = 0;
+    while (curNode1 || curNode2) {
+        curNode1 = curNode1 || { value: 0 };
+        curNode2 = curNode2 || { value: 0 };
+        let val = carryOver
+            + curNode1.value
+            + curNode2.value;
+        carryOver = Math.floor(val / 10);
+        const newNode = { value: val % 10, next: null }
+        newNodes.push(newNode);
+        curNode1 = curNode1.next;
+        curNode2 = curNode2.next;
+    }
+    if (carryOver) {
+        newNodes.push({ value: carryOver, next: null });
+    }
+    for (let i = 0; i < newNodes.length - 1; i++) {
+        newNodes[i].next = newNodes[i+1];
+    }
+
+    return newNodes[0];
+}
+
+describe('sum list', () => {
+    it('should sum list of the same length', () => {
+        const n1 = { value: 1, next: null };
+        const n2 = { value: 2, next: null };
+        const n3 = { value: 3, next: null };
+        n1.next = n2;
+        n2.next = n3;
+
+        const n4 = { value: 1, next: null };
+        const n5 = { value: 2, next: null };
+        const n6 = { value: 3, next: null };
+        n4.next = n5;
+        n5.next = n6;
+        const newList = sumList(n1, n4);
+        expect(newList.value).to.equal(2);
+        expect(newList.next.value).to.equal(4);
+        expect(newList.next.next.value).to.equal(6);
+    });
+    it('should sum list of the different length', () => {
+        const n1 = { value: 1, next: null };
+        const n2 = { value: 2, next: null };
+        const n3 = { value: 3, next: null };
+        n1.next = n2;
+        n2.next = n3;
+
+        const n4 = { value: 1, next: null };
+        const n5 = { value: 2, next: null };
+        const n6 = { value: 3, next: null };
+        const n7 = { value: 9, next: null };
+        n4.next = n5;
+        n5.next = n6;
+        n6.next = n7;
+        const newList = sumList(n1, n4);
+        expect(newList.value).to.equal(2);
+        expect(newList.next.value).to.equal(4);
+        expect(newList.next.next.value).to.equal(6);
+        expect(newList.next.next.next.value).to.equal(9);
+    });
+});
