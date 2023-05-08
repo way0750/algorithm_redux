4d66346e29d1a9f1449695a7f9ca81a91bd01d6b
Huiqiang Huang <bruce0750@gmail.com>
2023-05-09 03:36:31 +0700

    add implementation

--- algorithms_3/data_structures/linkedlist/ReturnKthToLast.ts
+++ algorithms_3/data_structures/linkedlist/ReturnKthToLast.ts
@@ -1,3 +1,31 @@
 /**
  * Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
+ * 
+ * 1: run throught the list twice, first run to find the length, second run to find the kth to last element
+ * time: O(n), space: O(1);
+ * 2: recursion: keep tracking the depth until hitting the last node
+ *  as you recursively call you pass down a cache object to communicate with lower stack
+ *  at the last node, you set cache.length = linkedlist length
+ *  then at each call, you compare current stack index to the length to k to see if you are at k
+ *  if index > kth, return null
+ *  if index = kth, return self
+ *  if index < kth, return what you got from lower stack
  */
+
+export function returnKthToLast (linkedlist, kth, cache = { length: null }, currentStack = 1) {
+    if (!linkedlist) {
+        cache.length = currentStack;
+        return null;
+    }
+    // get target rank
+    const returnedNode = returnKthToLast(linkedlist.next, kth, cache, currentStack + 1);
+    const targetRank = cache.length - kth;
+    if (currentStack === targetRank) {
+        return linkedlist;
+    } else if (currentStack > targetRank) {
+        return null;
+    } else {
+        return returnedNode;
+    }
+}
+
