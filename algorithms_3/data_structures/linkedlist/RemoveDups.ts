b540b9583f20457ef1b7a7ac2c87ecf7dcb41c79
Huiqiang Huang <bruce0750@gmail.com>
2023-05-09 03:13:29 +0700

    add tests

--- algorithms_3/data_structures/linkedlist/RemoveDups.ts
+++ algorithms_3/data_structures/linkedlist/RemoveDups.ts
@@ -18,3 +18,33 @@ export function removeDup (linkedList, cache = {}) {
         return linkedList;
     }
 }
+
+describe('reove dups', () => {
+    it('should not remove anything', () => {
+        const n1 = { value: 1, next: null };
+        const n2 = { value: 2, next: null };
+        const n3 = { value: 3, next: null };
+        const n4 = { value: 4, next: null };
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        removeDup(n1);
+        expect(n1.next).to.equal(n2);
+        expect(n2.next).to.equal(n3);
+        expect(n3.next).to.equal(n4);
+        expect(n4.next).to.equal(null);
+    });
+    it('should not remove anything', () => {
+        const n1 = { value: 1, next: null };
+        const n2 = { value: 2, next: null };
+        const n3 = { value: 3, next: null };
+        const n4 = { value: 3, next: null };
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        removeDup(n1);
+        expect(n1.next).to.equal(n2);
+        expect(n2.next).to.equal(n3);
+        expect(n3.next).to.equal(null);
+    });
+});
