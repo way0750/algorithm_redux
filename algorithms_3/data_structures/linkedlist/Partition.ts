02422e9463ca0750c32d74404993b8c80b3eec1d
Huiqiang Huang <bruce0750@gmail.com>
2023-05-09 05:11:14 +0700

    add new challenge

--- algorithms_3/data_structures/linkedlist/Partition.ts
+++ algorithms_3/data_structures/linkedlist/Partition.ts
@@ -4,4 +4,14 @@
  * only need to be after the elements less than x (see below).The partition element x can appear
  * anywhere in the "right partition"; it does not need to appear between the left and right partitions.
  * 
+ * maintain two pointers, swabNode and currNode
+ * whenever currNode runs into a node with value smaller than x, swab with swabNode, then move swabNode
+ *  further by one node
+ * 
+ * time: O(n), space: O(1);
+ * 
  */
+
+export function partition (list, partV) {
+    
+}
