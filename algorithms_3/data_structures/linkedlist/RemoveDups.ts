7d0384efce946177707dab58916cd2aa2abbbdc9
Huiqiang Huang <bruce0750@gmail.com>
2023-05-09 03:08:34 +0700

    add implementation

--- algorithms_3/data_structures/linkedlist/RemoveDups.ts
+++ algorithms_3/data_structures/linkedlist/RemoveDups.ts
@@ -1,3 +1,20 @@
 /**
  * Remove Dups: Write code to remove duplicates from an unsorted linked list.
+ * 
+ * so just maintain a cache to keep track of values you have encountered
+ * then remove duplicated one as you run into them
+ * time: O(n), space: O(n);
  */
+
+export function removeDup (linkedList, cache = {}) {
+    if (!linkedList) {
+        return linkedList;
+    }
+    if (cache[linkedList.value]) {
+        return removeDup(linkedList.next, cache);
+    } else {
+        cache[linkedList.value] = true;
+        linkedList.next = removeDup(linkedList.next, cache);
+        return linkedList;
+    }
+}
