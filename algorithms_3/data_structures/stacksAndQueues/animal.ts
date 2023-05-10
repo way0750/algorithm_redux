adcdecec5e5dd98122adaf8357c7ce929b5a7ba6
Huiqiang Huang <bruce0750@gmail.com>
2023-05-11 06:04:33 +0700

    add new implementation

--- algorithms_3/data_structures/stacksAndQueues/animal.ts
+++ algorithms_3/data_structures/stacksAndQueues/animal.ts
@@ -5,5 +5,14 @@
  * dog or a cat (and will receive the oldest animal of that type). They cannot select which
  * specific animal they would like. Create the data structures to maintain this system and
  * implement operations such as enqueue, dequeueAny, dequeueDog, and dequeueCat. You may use the
- * built-in Linked L is t data structure.
+ * built-in Linked List data structure.
+ * 
+ * build a special doublely linked list:
+ *  between animals two pointer
+ *  each node also maintain another pointer pointing to the next node of the same type
  */
+
+export class AnimalQueue {
+    // two linked list, one for cat, one for dog
+    // in each node just need to maintain a time property, done
+}
