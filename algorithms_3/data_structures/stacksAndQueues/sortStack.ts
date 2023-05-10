edfafa7d1e7d904004dc1ca922c4a828172193e9
Huiqiang Huang <bruce0750@gmail.com>
2023-05-11 05:11:05 +0700

    add new challenge

--- algorithms_3/data_structures/stacksAndQueues/sortStack.ts
+++ algorithms_3/data_structures/stacksAndQueues/sortStack.ts
@@ -3,4 +3,14 @@
  * You can use an additional temporary stack, but you may not copy the elements into any other data
  * structure (such as an array). The stack supports the following operations: push, pop, peek, and
  * isEmpty.
+ * 
+ * have two stacks
+ * values: []
+ * temp: []
+ * keep on pushing values to values[]
+ *  also compare before puttting it in
+ *  if value is larger than the top stack val
+ *    then keep on popping values out and put into the temp one until reaching either the end or
+ *      reaching a value that is bigger
+ *      then push the new val into the values[], then push all the value from temp[] back to the values
  */
