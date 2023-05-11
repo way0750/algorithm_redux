0d60f10bdfe1c76a21a1a2adf46905a781937a47
Huiqiang Huang <bruce0750@gmail.com>
2023-05-12 05:23:44 +0700

    add spec

--- algorithms_3/data_structures/treeAndGraph/MinimalTree.ts
+++ algorithms_3/data_structures/treeAndGraph/MinimalTree.ts
@@ -16,5 +16,30 @@
  */
 
 export function makeBinarySearchTree (arr) {
-    
+    if (!arr.length) {
+        return null;
+    }
+    const midIndex = Math.floor(arr.length/2);
+    const leftSubArray = arr.slice(0, midIndex);
+    const rightSubArray = arr.slice(midIndex+1);
+    return {
+        value: arr[midIndex],
+        left: makeBinarySearchTree(leftSubArray),
+        right: makeBinarySearchTree(rightSubArray),
+    };
 }
+
+describe('make binary search tree', () => {
+    it('should make tree with 1,2,3,4,5,6,7', () => {
+        const arr = [1,2,3,4,5,6,7];
+        const tree = makeBinarySearchTree(arr);
+        expect(tree.value).to.equal(4);
+        expect(tree.left.value).to.equal(2);
+        expect(tree.left.left.value).to.equal(1);
+        expect(tree.left.right.value).to.equal(3);
+
+        expect(tree.right.value).to.equal(6);
+        expect(tree.right.left.value).to.equal(5);
+        expect(tree.right.right.value).to.equal(7);
+    });
+});
