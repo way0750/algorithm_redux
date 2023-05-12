b84199c6dd421abee8183dd8aa24d8caf21ddf35
Huiqiang Huang <bruce0750@gmail.com>
2023-05-13 03:10:05 +0700

    add spec

--- algorithms_3/data_structures/treeAndGraph/ValidateBST.ts
+++ algorithms_3/data_structures/treeAndGraph/ValidateBST.ts
@@ -23,26 +23,75 @@
 
 
 export function checkBSTValues(node) {
-    let leftSubTree;
+    const orderedVals = [node.value];
     if (node.left) {
-        leftSubTree = validateBST(node.left);
+        const leftSubTree = checkBSTValues(node.left);
         if (leftSubTree[1] > node.value) {
             return [Infinity, -Infinity];
         }
+        orderedVals.unshift(leftSubTree[0]);
     }
 
-    let rightSubTree;
     if (node.right) {
-        rightSubTree = validateBST(node.right);
+        const rightSubTree = checkBSTValues(node.right);
         if (node.value > rightSubTree[0]) {
             return [Infinity, -Infinity];
         }
+        orderedVals.push(rightSubTree[1]);
     }
 
-    return [leftSubTree[0], rightSubTree[1]];
+    return [
+        orderedVals[0], // min
+        orderedVals[orderedVals.length-1] // max
+    ];
 }
 
 function validateBST(node) {
     const [min, max] = checkBSTValues(node);
     return min <= max;
 }
+
+describe('validate BST', () => {
+    it('should return true', () => {
+        const node1 = { value: 1, left: null, right: null };
+        expect(validateBST(node1)).to.equal(true);
+    });
+    it('should return true', () => {
+        const node1 = { value: 1, left: null, right: null };
+        const node2 = { value: 2, left: null, right: null };
+        const node3 = { value: 3, left: null, right: null };
+        const node4 = { value: 4, left: null, right: null };
+        const node5 = { value: 5, left: null, right: null };
+        const node6 = { value: 6, left: null, right: null };
+        const node7 = { value: 7, left: null, right: null };
+
+        node4.left = node2;
+        node4.right = node6;
+        node2.left = node1;
+        node2.right = node3;
+
+        node6.left = node5;
+        node6.right = node7;
+
+        expect(validateBST(node4)).to.equal(true);
+    });
+    it('should return false', () => {
+        const node1 = { value: 1, left: null, right: null };
+        const node2 = { value: 2, left: null, right: null };
+        const node3 = { value: 3, left: null, right: null };
+        const node4 = { value: 44, left: null, right: null };
+        const node5 = { value: 5, left: null, right: null };
+        const node6 = { value: 6, left: null, right: null };
+        const node7 = { value: 7, left: null, right: null };
+
+        node4.left = node2;
+        node4.right = node6;
+        node2.left = node1;
+        node2.right = node3;
+
+        node6.left = node5;
+        node6.right = node7;
+
+        expect(validateBST(node4)).to.equal(false);
+    });
+});
