204954e2779073b4137f9a2f3f15827feeb861fa
Huiqiang Huang <bruce0750@gmail.com>
2023-05-17 02:44:33 +0700

    add new challenge

--- algorithms_3/data_structures/treeAndGraph/RandomNode.ts
+++ algorithms_3/data_structures/treeAndGraph/RandomNode.ts
@@ -4,4 +4,14 @@
  * which returns a random node from the tree. All nodes should be equally
  * likely to be chosen. Design and implement an algorithm for getRandomNode,
  * and explain how you would implement the rest of the methods.
+ * 
+ * assuming you can't delete node in the middle of the tree, you can only add
+ * and remove on the last level at the right most available position then use
+ * an array to implement this tree class:
+ * parent node to children:
+ *  parent node index * 2 then +1 for left +2 for right
+ * children to parent node:
+ *  (children index -1)/2 then floor it
+ * getRandomNode would just get a random index by math.random * length
  */
+
